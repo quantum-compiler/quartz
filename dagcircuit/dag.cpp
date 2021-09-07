d05253bb1a8eef2b21f6869c3290b0e461442e46
xumingkuan <xumingkuan0721@126.com>
2021-09-07 00:51:16 -0400

    Use std::unique_ptr for nodes and edges

--- dagcircuit/dag.cpp
+++ dagcircuit/dag.cpp
@@ -2,29 +2,27 @@
 #include <cassert>
 
 DAG::DAG(int _num_qubits, int _num_parameters)
-: num_qubits(_num_qubits), num_parameters(_num_parameters)
-{
+    : num_qubits(_num_qubits), num_parameters(_num_parameters) {
   // Initialize num_qubits qubits
   for (int i = 0; i < num_qubits; i++) {
-    DAGNode* node = new DAGNode();
+    auto node = std::make_unique<DAGNode>();
     node->type = DAGNode::input_qubit;
-    nodes.push_back(node);
-    outputs.push_back(node);
+    outputs.push_back(node.get());
+    nodes.push_back(std::move(node));
   }
   // Initialize num_parameters parameters
   for (int i = 0; i < num_parameters; i++) {
-    DAGNode* node = new DAGNode();
+    auto node = std::make_unique<DAGNode>();
     node->type = DAGNode::input_param;
-    nodes.push_back(node);
-    parameters.push_back(node);
+    parameters.push_back(node.get());
+    nodes.push_back(std::move(node));
   }
 }
 
-bool DAG::add_gate(const std::vector<int>& qubit_indices,
-                   const std::vector<int>& parameter_indices,
-                   const Gate* gate,
-                   int* output_para_index)
-{
+bool DAG::add_gate(const std::vector<int> &qubit_indices,
+                   const std::vector<int> &parameter_indices,
+                   const Gate *gate,
+                   int *output_para_index) {
   if (gate->get_num_qubits() != qubit_indices.size())
     return false;
   if (gate->get_num_parameters() != parameter_indices.size())
@@ -39,47 +37,38 @@ bool DAG::add_gate(const std::vector<int>& qubit_indices,
   for (auto para_idx : parameter_indices)
     if ((para_idx < 0) || (para_idx >= parameters.size()))
       return false;
-  DAGHyperEdge* edge = new DAGHyperEdge();
-  edges.push_back(edge);
+  auto edge = std::make_unique<DAGHyperEdge>();
   edge->gate = gate;
   for (auto qubit_idx : qubit_indices) {
     edge->input_nodes.push_back(outputs[qubit_idx]);
-    outputs[qubit_idx]->output_edges.push_back(edge);
+    outputs[qubit_idx]->output_edges.push_back(edge.get());
   }
   for (auto para_idx : parameter_indices) {
     edge->input_nodes.push_back(parameters[para_idx]);
-    parameters[para_idx]->output_edges.push_back(edge);
+    parameters[para_idx]->output_edges.push_back(edge.get());
   }
   if (gate->is_parameter_gate()) {
-    DAGNode* node = new DAGNode();
-    nodes.push_back(node);
+    auto node = std::make_unique<DAGNode>();
     node->type = DAGNode::internal_param;
-    node->input_edges.push_back(edge);
-    edge->output_nodes.push_back(node);
-    parameters.push_back(node);
+    node->input_edges.push_back(edge.get());
+    edge->output_nodes.push_back(node.get());
+    parameters.push_back(node.get());
     *output_para_index = parameters.size();
+    nodes.push_back(std::move(node));
   } else {
     assert(gate->is_quantum_gate());
     for (auto qubit_idx : qubit_indices) {
-      DAGNode* node = new DAGNode();
-      nodes.push_back(node);
+      auto node = std::make_unique<DAGNode>();
       node->type = DAGNode::internal_param;
-      node->input_edges.push_back(edge);
-      edge->output_nodes.push_back(node);
-      outputs[qubit_idx] = node; // Update outputs
+      node->input_edges.push_back(edge.get());
+      edge->output_nodes.push_back(node.get());
+      outputs[qubit_idx] = node.get(); // Update outputs
+      nodes.push_back(std::move(node));
     }
   }
+  edges.push_back(std::move(edge));
 }
 
-int DAG::get_num_qubits() const
-{
+int DAG::get_num_qubits() const {
   return num_qubits;
-}
-
-DAG::~DAG()
-{
-  for (auto node : nodes)
-    delete node;
-  for (auto edge : edges)
-    delete edge;
 }
--- dagcircuit/dag.h
+++ dagcircuit/dag.h
@@ -6,21 +6,20 @@
 #include "../gate/gate.h"
 
 class DAG {
-public:
+ public:
   DAG(int _num_qubits, int _num_parameters);
-  ~DAG();
-  std::vector<DAGNode*> nodes;
-  std::vector<DAGHyperEdge*> edges;
+  std::vector<std::unique_ptr<DAGNode>> nodes;
+  std::vector<std::unique_ptr<DAGHyperEdge>> edges;
   // The gates' information is owned by edges.
-  std::vector<DAGNode*> outputs;
-  std::vector<DAGNode*> parameters;
-  bool add_gate(const std::vector<int>& qubit_indices,
-                const std::vector<int>& parameter_indices,
-                const Gate* gate,
-                int* output_para_index);
-  bool evaluate(const std::vector<ComplexType>& input_dis,
-                const std::vector<ParamType>& parameters,
-                std::vector<ComplexType>& output_dis) const;
+  std::vector<DAGNode *> outputs;
+  std::vector<DAGNode *> parameters;
+  bool add_gate(const std::vector<int> &qubit_indices,
+                const std::vector<int> &parameter_indices,
+                const Gate *gate,
+                int *output_para_index);
+  bool evaluate(const std::vector<ComplexType> &input_dis,
+                const std::vector<ParamType> &parameters,
+                std::vector<ComplexType> &output_dis) const;
   int get_num_qubits() const;
   int num_qubits, num_parameters;
 };

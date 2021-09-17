0a7f4a8889e522c2f281366f31a430ac32061aad
xumingkuan <xumingkuan0721@126.com>
2021-09-17 16:58:40 -0400

    Code skeleton for verifier

---
+++ verifier/verifier.h
@@ -0,0 +1,10 @@
+#include "../context/context.h"
+#include "../dag/dag.h"
+
+// Verify if two circuits are equivalent.
+class Verifier {
+ public:
+  bool equivalent(Context *ctx, DAG *circuit1, DAG *circuit2);
+  // On-the-fly equivalence checking while generating circuits
+  bool equivalent_on_the_fly(Context *ctx, DAG *circuit1, DAG *circuit2);
+};

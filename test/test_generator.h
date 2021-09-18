441fc005b2e0d9218a776541158f566b0057e5f4
Zhihao Jia <zhihao@fb.com>
2021-09-18 19:44:35 +0000

    [Generator] update constructor

--- test/test_generator.h
+++ test/test_generator.h
@@ -6,9 +6,9 @@ void test_generator() {
   std::vector<GateType> support_gates =
       {GateType::x, GateType::y, GateType::rx, GateType::ry, GateType::rz};
   Context ctx(support_gates);
-  Generator generator;
+  Generator generator(&ctx);
   std::unordered_map<DAGHashType, std::unordered_set<DAG*> > dataset;
-  generator.generate(&ctx, 2, 2, 3, dataset);
+  generator.generate(2, 2, 3, dataset);
   for (auto &it : dataset) {
     std::cout << std::hex << it.first << ":" << std::endl;
     for (auto &dag : it.second) {

9e5e2ace431d813148881e3b6e9eaae8deaa6024
xumingkuan <xumingkuan0721@126.com>
2021-09-19 21:57:30 -0400

    Disable verifier

--- test/test_generator.h
+++ test/test_generator.h
@@ -2,6 +2,8 @@
 
 #include "../generator/generator.h"
 
+#include <chrono>
+
 void test_generator() {
   std::vector<GateType> support_gates =
       {GateType::x, GateType::y, GateType::rx, GateType::ry, GateType::rz,
@@ -9,17 +11,21 @@ void test_generator() {
   Context ctx(support_gates);
   Generator generator(&ctx);
   std::unordered_map<DAGHashType, std::unordered_set<DAG *> > dataset;
-  generator.generate(2, 2, 3, dataset);
+  auto start = std::chrono::system_clock::now();
+  generator.generate(3, 2, 4, dataset);
+  auto end = std::chrono::system_clock::now();
   for (auto &it : dataset) {
     if (it.second.size() <= 1) {
       continue;
     }
-    std::cout << std::hex << it.first << ":" << std::endl;
+    /*std::cout << std::hex << it.first << ":" << std::endl;
     for (auto &dag : it.second) {
       std::cout << dag->to_string();
-    }
+    }*/
   }
   std::cout << std::dec << "Circuits with " << dataset.size()
-            << " different hash values are found."
+            << " different hash values are found in "
+            << (double) std::chrono::duration_cast<std::chrono::milliseconds>(
+                end - start).count() / 1000.0 << " seconds."
             << std::endl;
 }
--- verifier/verifier.cpp
+++ verifier/verifier.cpp
@@ -3,6 +3,8 @@
 bool Verifier::equivalent_on_the_fly(Context *ctx,
                                      DAG *circuit1,
                                      DAG *circuit2) {
+  // Disable the verifier.
+  return false;
   // Aggressively assume circuits with the same hash values are equivalent.
   return circuit1->hash(ctx) == circuit2->hash(ctx);
 }

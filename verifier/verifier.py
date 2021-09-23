aa5f882e105a8c12a3cad8be5a5e30e85dfa55c2
xumingkuan <xumingkuan0721@126.com>
2021-09-23 14:44:22 -0400

    apply_matrix() and input_distribution() in verifier.py

--- verifier/verifier.py
+++ verifier/verifier.py
@@ -9,9 +9,10 @@ Angles are represented with two real numbers, s and c, satisfying s*s+c*c=1
 import z3
 import math
 
+
 # functions for generating z3 constraints
 
-def eq(A, B):
+def eq_matrix(A, B):
     assert len(A) == len(B)
     assert all(len(ra) == len(rb) for ra, rb in zip(A, B))
     assert all(len(z) == 2 for M in (A, B) for r in M for z in r)
@@ -21,6 +22,17 @@ def eq(A, B):
         for za, zb in zip(ra, rb)
         for x, y in zip(za, zb)
     ])
+
+
+def eq_vector(A, B):
+    assert len(A) == len(B)
+    assert all(len(ra) == len(rb) for ra, rb in zip(A, B))
+    return z3.And([
+        za == zb
+        for ra, rb in zip(A, B)
+        for za, zb in zip(ra, rb)
+    ])
+
 
 def matmul(A, B):
     n = len(A)
@@ -40,8 +52,60 @@ def matmul(A, B):
         matrix.append(row)
     return matrix
 
+
+# Apply |mat| at |qubit_indices| on the distribution vector |vec|
+def apply_matrix(vec, mat, qubit_indices):
+    # See also: Vector::apply_matrix() in math/vector.cpp
+    S = len(vec)
+    n = len(mat)
+    n0 = len(qubit_indices)
+    assert 1 <= n0 <= 2
+    assert (1 << n0) == n
+    assert n <= S
+    assert S % n == 0
+    assert all(len(row) == n for row in mat)
+    assert all(1 <= (1 << index) < S for index in qubit_indices)
+    result_vec = [None] * S
+    for i in range(S):
+        already_applied = False
+        for index in qubit_indices:
+            if i & (1 << index):
+                already_applied = True
+        if already_applied:
+            continue
+
+        current_indices = list()
+        for j in range(n):
+            current_index = i
+            for k in range(n0):
+                if j & (1 << k):
+                    current_index ^= (1 << qubit_indices[k])
+            current_indices.append(current_index)
+
+        # matrix * vector
+        for r in range(n):
+            val_real = 0
+            val_imag = 0
+            for k in range(n):
+                val_real += mat[r][k][0] * vec[current_indices[k]][0] - mat[r][k][1] * vec[current_indices[k]][1]
+                val_imag += mat[r][k][0] * vec[current_indices[k]][1] + mat[r][k][1] * vec[current_indices[k]][0]
+            result_vec[current_indices[r]] = (val_real, val_imag)
+    return result_vec
+
+
+def input_distribution(num_qubits, solver):
+    vec_size = 1 << num_qubits
+    real_part = z3.RealVector('r', vec_size)
+    imag_part = z3.RealVector('i', vec_size)
+    # A quantum state requires the sum of modulus of all numbers to be 1.
+    sum_modulus = sum([x * x for x in real_part]) + sum([x * x for x in imag_part])
+    solver.add(sum_modulus == 1)
+    return list(zip(real_part, imag_part))
+
+
 def angle(s, c):
-    return s*s + c*c == 1
+    return s * s + c * c == 1
+
 
 # parameter gates
 def Add(a, b):
@@ -49,7 +113,8 @@ def Add(a, b):
     assert len(b) == 2
     cos_a, sin_a = a
     cos_b, sin_b = b
-    return (cos_a*cos_b-sin_a*sin_b, sin_a*cos_b+cos_a*sin_b)
+    return (cos_a * cos_b - sin_a * sin_b, sin_a * cos_b + cos_a * sin_b)
+
 
 # quantum gates
 
@@ -59,11 +124,13 @@ def RX(theta):
     return [[(cos_theta, 0), (0, -sin_theta)],
             [(0, -sin_theta), (cos_theta, 0)]]
 
+
 def RY(theta):
     assert len(theta) == 2
     cos_theta, sin_theta = theta
     return [[(cos_theta, 0), (-sin_theta, 0)],
             [(sin_theta, 0), (cos_theta, 0)]]
+
 
 def RZ(theta):
     # e ^ {i * theta} = cos theta + i sin theta
@@ -72,11 +139,13 @@ def RZ(theta):
     return [[(cos_theta, -sin_theta), (0, 0)],
             [(0, 0), (cos_theta, sin_theta)]]
 
+
 def U1(theta):
     assert len(theta) == 2
     cos_theta, sin_theta = theta
     return [[(1, 1), (0, 0)],
             [(0, 0), (cos_theta, sin_theta)]]
+
 
 if __name__ == "__main__":
     s1, c1, s2, c2 = z3.Reals('s1 c1 s2 c2')
@@ -84,7 +153,11 @@ if __name__ == "__main__":
     slv = z3.Solver()
     slv.add(angle(s1, c1))
     slv.add(angle(s2, c2))
-    slv.add(z3.Not(eq(
-        matmul(RX((c1, s1)), RX((c2, s2))),
-        RX(Add((c1,s1), (c2,s2))))))
+    # slv.add(z3.Not(eq_matrix(
+    #     matmul(RX((c1, s1)), RX((c2, s2))),
+    #     RX(Add((c1,s1), (c2,s2))))))
+    vec = input_distribution(1, slv)
+    output_vec1 = apply_matrix(apply_matrix(vec, RX((c1, s1)), [0]), RX((c2, s2)), [0])
+    output_vec2 = apply_matrix(vec, RX(Add((c1, s1), (c2, s2))), [0])
+    slv.add(z3.Not(eq_vector(output_vec1, output_vec2)))
     print(slv.check())

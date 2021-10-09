83c199da1625149577769ab9ff8a148d1e4fc4b0
Sina Lin <olivialsn@gmail.com>
2021-10-09 12:10:48 -0700

    minor fix

--- gate/s.h
+++ gate/s.h
@@ -5,7 +5,7 @@
 class SGate : public Gate {
  public:
   SGate() : Gate(GateType::s, 1/*num_qubits*/, 0/*num_parameters*/),
-            mat({{1, 0}, {0, i}}) {}
+            mat({{1, 0}, {0, 1.0i}}) {}
 
   MatrixBase *get_matrix() override {
     return &mat;

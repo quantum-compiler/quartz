e4f6a939fb1c29bb9be587211efec157162942e6
zikun-li <lizikunzk@gmail.com>
2021-10-11 00:11:27 -0700

    fix tdg gate

--- gate/tdg.h
+++ gate/tdg.h
@@ -2,10 +2,10 @@
 
 #include "gate.h"
 
-class TGate : public Gate {
+class TDGGate : public Gate {
 public:
-  TGate()
-      : Gate(GateType::t, 1 /*num_qubits*/, 0 /*num_parameters*/),
+  TDGGate()
+      : Gate(GateType::tdg, 1 /*num_qubits*/, 0 /*num_parameters*/),
         mat({{1, 0}, {0, std::sqrt(2) / 2 * (1.0 - 1.0i)}}) {}
 
   MatrixBase *get_matrix() override { return &mat; }

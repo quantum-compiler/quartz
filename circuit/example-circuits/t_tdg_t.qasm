571ec2930d58ef72496e2c7c2b3a8ae1d319e63f
zikun-li <lizikunzk@gmail.com>
2021-11-01 19:17:14 -0700

    test_context_shift

---
+++ circuit/example-circuits/t_tdg_t.qasm
@@ -0,0 +1,6 @@
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[1];
+t q[0];
+tdg q[0];
+t q[0];
--- test/test_context_shift.cpp
+++ test/test_context_shift.cpp
@@ -2,11 +2,20 @@
 
 int main() {
   Context src_ctx({GateType::input_qubit, GateType::input_param, GateType::t,
-                   GateType::tdg, GateType::h, GateType::cx});
-  Context dst_ctx({GateType::input_qubit, GateType::input_param, GateType::u1,
-                   GateType::u2, GateType::u3, GateType::cx});
-  RuleParser rule_parser(
-      {"t q0 = u1 q0 0.25pi", "h q0 = u2 q0 0 pi", "tdg q0 = u2 q0 -0.25pi"});
-  test_context_shift("circuit/example-circuits/barenco_tof_3.qasm", &src_ctx,
+                   GateType::tdg});
+  Context dst_ctx({
+      GateType::input_qubit,
+      GateType::input_param,
+      GateType::u1,
+  });
+  RuleParser rule_parser({
+      "t q0 = u1 q0 0.25pi",    // t -> u1
+      "h q0 = u2 q0 0 pi",      // h-> u2
+      "tdg q0 = u1 q0 -0.25pi", // tdg -> u1
+      "ccz q0 q1 q2 = cx q1 q2; tdg q2; cx q0 q2; t q2; cx q1 q2; tdg q2; cx "
+      "q0 q2; cx q0 q1; tdg q1; cx q0 q1; t q0; t q1; t q2;" // ccx -> t, tdg,
+                                                             // cx
+  });
+  test_context_shift("circuit/example-circuits/t_tdg_t.qasm", &src_ctx,
                      &dst_ctx, &rule_parser);
 }

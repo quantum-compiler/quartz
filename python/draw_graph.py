6ce290a96df81577e5b533820ce29c2747c00cf6
zikun-li <lizikunzk@gmail.com>
2021-11-06 02:14:25 +0000

    draw circuit

---
+++ python/draw_graph.py
@@ -0,0 +1,12 @@
+import sys
+from qiskit import QuantumCircuit, transpile
+
+
+def draw_from_qasm(qasm_file, **kwargs):
+    circuit = QuantumCircuit.from_qasm_file(qasm_file)
+    print(circuit.draw())
+    circuit.draw(circuit, kwargs)
+
+
+if __name__ == '__main__':
+    draw_from_qasm(sys.argv[1], fliename=sys.argv[2])

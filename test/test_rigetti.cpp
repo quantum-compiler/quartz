89c25ada9f7c0ec6475d88c096ecb91420dbf154
zikun-li <lizikunzk@gmail.com>
2021-11-13 16:38:21 -0800

    merge cz and h before searching under rigetti gate set

--- test/test_rigetti.cpp
+++ test/test_rigetti.cpp
@@ -34,7 +34,7 @@ void parse_args(char **argv, int argc, bool &simulated_annealing,
 
 int main(int argc, char **argv) {
   std::string input_fn, output_fn;
-  std::string eqset_fn = "build/bfs_verified_simplified.json";
+  std::string eqset_fn = "build/bfs_verified_simplified_rigetti.json";
   bool simulated_annealing = false;
   bool early_stop = false;
   parse_args(argv, argc, simulated_annealing, early_stop, input_fn, output_fn,
@@ -65,23 +65,33 @@ int main(int argc, char **argv) {
       GateType::rz, xfer_pair.first, xfer_pair.second);
   std::cout << "gate count after toffoli flip: " << new_graph->total_cost()
             << std::endl;
-  //   new_graph->to_qasm(input_fn + ".toffoli_flip", false, false);
+
+  // Convert cx to cz and merge h gates
+  RuleParser cx_2_cz({"cx q0 q1 = h q1; cz q0 q1; h q1;"});
+  Context cz_ctx({GateType::rz, GateType::h, GateType::x, GateType::cz,
+                  GateType::add, GateType::input_qubit, GateType::input_param});
+  auto union_ctx_0 = union_contexts(&cz_ctx, &dst_ctx);
+  TASOGraph::Graph *graph_before_h_cz_merge = new_graph->context_shift(
+      &dst_ctx, &cz_ctx, &union_ctx_0, &cx_2_cz, false);
+  TASOGraph::Graph *graph_after_h_cz_merge = graph_before_h_cz_merge->optimize(
+      0.999, 0, false, &union_ctx_0, "build/h_cz_merge.json",
+      simulated_annealing, false, /*rotation_merging_in_searching*/ true,
+      GateType::rz);
+  graph_after_h_cz_merge->to_qasm(
+      "circuit/voqc-benchmarks/after_h_cz_merge.qasm", false, false);
 
   // Shift the context to Rigetti Agave
-  RuleParser rules(
-      {"h q0 = rx q0 pi; rz q0 0.5pi; rx q0 0.5pi; rz q0 -0.5pi;",
-       "cx q0 q1 = rx q1 pi; rz q1 0.5pi; rx q1 0.5pi; rz q1 -0.5pi; cz q0 q1; "
-       "rx q1 pi; rz q1 0.5pi; rx q1 0.5pi; rz q1 -0.5pi;",
-       "x q0 = rx q0 pi;"});
+  RuleParser rules({"h q0 = rx q0 pi; rz q0 0.5pi; rx q0 0.5pi; rz q0 -0.5pi;",
+                    "x q0 = rx q0 pi;"});
   Context rigetti_ctx({GateType::rx, GateType::rz, GateType::cz, GateType::add,
                        GateType::input_qubit, GateType::input_param});
-  auto union_ctx_1 = union_contexts(&rigetti_ctx, &dst_ctx);
-  TASOGraph::Graph *graph_rigetti = new_graph->context_shift(
-      &dst_ctx, &rigetti_ctx, &union_ctx_1, &rules, false);
+  auto union_ctx_1 = union_contexts(&rigetti_ctx, &union_ctx_0);
+  TASOGraph::Graph *graph_rigetti = graph_after_h_cz_merge->context_shift(
+      &cz_ctx, &rigetti_ctx, &union_ctx_1, &rules, false);
 
   // Optimization
   TASOGraph::Graph *graph_after_search = graph_rigetti->optimize(
-      0.999, 0, false, &dst_ctx, eqset_fn, simulated_annealing, early_stop,
+      0.999, 0, false, &union_ctx_1, eqset_fn, simulated_annealing, early_stop,
       /*rotation_merging_in_searching*/ true, GateType::rz);
   std::cout << "gate count after optimization: "
             << graph_after_search->total_cost() << std::endl;

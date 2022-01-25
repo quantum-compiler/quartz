b4e7b227307232c91c1675d46174b10d215826b1
zikun-li <lizikunzk@gmail.com>
2022-01-25 09:47:20 -0800

    Add a test for apply_xfer API

---
+++ test/test_apply_xfer.cpp
@@ -0,0 +1,39 @@
+#include "quartz/tasograph/substitution.h"
+#include "quartz/tasograph/tasograph.h"
+#include "quartz/context/context.h"
+#include "quartz/parser/qasm_parser.h"
+
+using namespace quartz;
+
+int main() {
+	Context ctx({GateType::input_qubit, GateType::input_param, GateType::h});
+
+	// Construct circuit graph from qasm file
+	QASMParser qasm_parser(&ctx);
+	DAG *dag = nullptr;
+	if (!qasm_parser.load_qasm("circuit/example-circuits/h_h.qasm", dag)) {
+		std::cout << "Parser failed" << std::endl;
+		return 0;
+	}
+	Graph graph(&ctx, *dag);
+
+	EquivalenceSet eqs;
+	// Load equivalent dags from file
+	auto start = std::chrono::steady_clock::now();
+	if (!eqs.load_json(&ctx, "bfs_verified_simplified.json")) {
+		std::cout << "Failed to load equivalence file." << std::endl;
+		assert(false);
+	}
+
+	// Get xfer from the equivalent set
+	auto all_xfers = eqs.get_all_equivalence_sets();
+	GraphXfer *xfer =
+	    GraphXfer::create_GraphXfer(&ctx, all_xfers[0][1], all_xfers[0][0]);
+
+	std::vector< Op > ops;
+	graph.all_ops(ops);
+	for (auto it = ops.begin(); it != ops.end(); ++it) {
+		bool xfer_ok = graph.xfer_appliable(xfer, &(*it));
+		std::cout << (int)xfer_ok << std::endl;
+	}
+}

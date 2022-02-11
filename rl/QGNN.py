034ad47594c39c52fdf39a397af558c4ac601e1d
Sina Lin <olivialsn@gmail.com>
2022-02-11 10:45:15 -0800

    add train example code

--- rl/QGNN.py
+++ rl/QGNN.py
@@ -64,3 +64,78 @@ class QGNN(nn.Module):
         h = F.relu(h)
         h = self.conv5(g, h)
         return h
+
+   
+def train(g, model):
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    all_logits = []
+    best_val_acc = 0
+    best_test_acc = 0
+
+    features = g.ndata['gate_type']
+
+    labels = g.ndata['label']
+    train_mask = g.ndata['train_mask']
+    val_mask = g.ndata['val_mask']
+    test_mask = g.ndata['test_mask']
+    for e in range(20):
+        # Forward
+        logits = model(g)
+
+        # Compute prediction
+        ####TODO######
+        pred = logits.argmax(1)
+
+        # Compute loss
+        # Note that we should only compute the losses of the nodes in the training set,
+        # i.e. with train_mask 1.
+        loss = F.cross_entropy(logits[train_mask], labels[train_mask])
+
+        # Compute accuracy on training/validation/test
+        train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
+        val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
+        test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
+
+        # Save the best validation accuracy and the corresponding test accuracy.
+        if best_val_acc < val_acc:
+            best_val_acc = val_acc
+            best_test_acc = test_acc
+
+        # Backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        all_logits.append(logits.detach())
+
+        if e % 5 == 0:
+            print('In epoch {}, loss: {:.3f}, val acc: {:.3f} (best {:.3f}), test acc: {:.3f} (best {:.3f})'.format(
+                e, loss, val_acc, best_val_acc, test_acc, best_test_acc))
+   
+
+    
+#import dgl.data
+#dataset = dgl.data.CoraGraphDataset()
+#g = dataset[0]
+src_id = [0,3,1,1,5]
+dst_id = [1,1,2,4,0]
+src_idx = [0,0,0,1,0]
+dst_idx = [0,1,0,0,0]
+node_gate_tp = [1,0,2,3,4,1]
+src_id2 = src_id + dst_id
+dst_id2 = dst_id + src_id
+src_idx2 = src_idx + dst_idx
+dst_idx2 = dst_idx + src_idx
+reverse = [0] * len(src_id) + [1] * len(src_id)
+g = dgl.graph((torch.tensor(src_id2), torch.tensor(dst_id2)))
+g.edata['src_idx'] = torch.tensor(src_idx2)
+g.edata['dst_idx'] = torch.tensor(dst_idx2)
+g.edata['reversed'] = torch.tensor(reverse)
+g.ndata['gate_type'] = torch.tensor(node_gate_tp)
+
+g.ndata['label'] = torch.tensor([1,1,1,1,1,1])
+g.ndata['train_mask'] = torch.tensor([1,1,1,1,0,0]) 
+g.ndata['val_mask'] = torch.tensor([0,0,0,0,1,0]) 
+g.ndata['test_mask'] = torch.tensor([0,0,0,0,0,1]) 
+    
+model = QGNN(5, 16, 200, 16)
+train(g, model)

f7ac6cc1b930d5f95599cad2d222c74f8cd9ebd3
Antony <meiyixuan2000@126.com>
2022-03-12 21:03:34 +0800

    add qubit idx in Graph's Edge

--- src/quartz/tasograph/tasograph.h
+++ src/quartz/tasograph/tasograph.h
@@ -19,218 +19,273 @@ namespace quartz {
 
 #define eps 1e-6
 
-bool equal_to_2k_pi(double d);
+    bool equal_to_2k_pi(double d);
 
-class Op {
-public:
-  Op(void);
-  Op(size_t _guid, Gate *_ptr) : guid(_guid), ptr(_ptr) {}
-  inline bool operator==(const Op &b) const {
-    if (guid != b.guid)
-      return false;
-    if (ptr != b.ptr)
-      return false;
-    return true;
-  }
-  inline bool operator!=(const Op &b) const {
-    if (guid != b.guid)
-      return true;
-    if (ptr != b.ptr)
-      return true;
-    return false;
-  }
-  inline bool operator<(const Op &b) const {
-    if (guid != b.guid)
-      return guid < b.guid;
-    if (ptr != b.ptr)
-      return ptr < b.ptr;
-    return false;
-  }
-  Op &operator=(const Op &op) {
-    guid = op.guid;
-    ptr = op.ptr;
-    return *this;
-  }
-  static const Op INVALID_OP;
+    class Op {
+    public:
+        Op(void);
 
-public:
-  size_t guid;
-  Gate *ptr;
-};
+        Op(size_t _guid, Gate *_ptr) : guid(_guid), ptr(_ptr) {}
 
-class OpCompare {
-public:
-  bool operator()(const Op &a, const Op &b) const {
-    if (a.guid != b.guid)
-      return a.guid < b.guid;
-    return a.ptr < b.ptr;
-  };
-};
+        inline bool operator==(const Op &b) const {
+            if (guid != b.guid)
+                return false;
+            if (ptr != b.ptr)
+                return false;
+            return true;
+        }
 
-class OpHash {
-public:
-  size_t operator()(const Op &a) const {
-    std::hash<size_t> hash_fn;
-    return hash_fn(a.guid) * 17 + hash_fn((size_t)(a.ptr));
-  }
-};
+        inline bool operator!=(const Op &b) const {
+            if (guid != b.guid)
+                return true;
+            if (ptr != b.ptr)
+                return true;
+            return false;
+        }
 
-class Pos {
-public:
-  Pos() {
-    op = Op();
-    idx = 0;
-  }
-  Pos(const Pos &b) {
-    op = b.op;
-    idx = b.idx;
-  }
-  inline bool operator<(const Pos &b) const {
-    if (op != b.op)
-      return op < b.op;
-    if (idx != b.idx)
-      return idx < b.idx;
-    return false;
-  }
-  Pos &operator=(const Pos &pos) {
-    op = pos.op;
-    idx = pos.idx;
-    return *this;
-  }
-  Pos(Op op_, int idx_) : op(op_), idx(idx_) {}
-  Op op;
-  int idx;
-};
+        inline bool operator<(const Op &b) const {
+            if (guid != b.guid)
+                return guid < b.guid;
+            if (ptr != b.ptr)
+                return ptr < b.ptr;
+            return false;
+        }
 
-inline bool operator==(const Pos &a, const Pos &b) {
-  if (a.op != b.op)
-    return false;
-  if (a.idx != b.idx)
-    return false;
-  return true;
-}
-inline bool operator!=(const Pos &a, const Pos &b) {
-  if (a.op != b.op)
-    return true;
-  if (a.idx != b.idx)
-    return true;
-  return false;
-}
+        Op &operator=(const Op &op) {
+            guid = op.guid;
+            ptr = op.ptr;
+            return *this;
+        }
 
-class PosHash {
-public:
-  size_t operator()(const Pos &a) const {
-    std::hash<size_t> hash_fn;
-    OpHash op_hash;
-    return op_hash(a.op) * 17 + hash_fn(a.idx);
-  }
-};
+        static const Op INVALID_OP;
 
-class PosCompare {
-public:
-  bool operator()(const Pos &a, const Pos &b) const {
-    if (a.op != b.op)
-      return a.op < b.op;
-    return a.idx < b.idx;
-  }
-};
+    public:
+        size_t guid;
+        Gate *ptr;
+    };
 
-class Tensor {
-public:
-  Tensor(void);
-  int idx;
-  Op op;
-};
+    class OpCompare {
+    public:
+        bool operator()(const Op &a, const Op &b) const {
+            if (a.guid != b.guid)
+                return a.guid < b.guid;
+            return a.ptr < b.ptr;
+        };
+    };
 
-struct Edge {
-  Edge(void);
-  Edge(const Op &_srcOp, const Op &_dstOp, int _srcIdx, int _dstIdx);
-  Op srcOp, dstOp;
-  int srcIdx, dstIdx;
-};
+    class OpHash {
+    public:
+        size_t operator()(const Op &a) const {
+            std::hash<size_t> hash_fn;
+            return hash_fn(a.guid) * 17 + hash_fn((size_t) (a.ptr));
+        }
+    };
 
-struct EdgeCompare {
-  bool operator()(const Edge &a, const Edge &b) const {
-    if (!(a.srcOp == b.srcOp))
-      return a.srcOp < b.srcOp;
-    if (!(a.dstOp == b.dstOp))
-      return a.dstOp < b.dstOp;
-    if (a.srcIdx != b.srcIdx)
-      return a.srcIdx < b.srcIdx;
-    if (a.dstIdx != b.dstIdx)
-      return a.dstIdx < b.dstIdx;
-    return false;
-  };
-};
+    class Pos {
+    public:
+        Pos() {
+            op = Op();
+            idx = 0;
+        }
 
-class GraphXfer;
+        Pos(const Pos &b) {
+            op = b.op;
+            idx = b.idx;
+        }
 
-class Graph {
-public:
-  Graph(Context *ctx);
-  Graph(Context *ctx, const DAG *dag);
-  Graph(const Graph &graph);
-  void add_edge(const Op &srcOp, const Op &dstOp, int srcIdx, int dstIdx);
-  bool has_edge(const Op &srcOp, const Op &dstOp, int srcIdx, int dstIdx) const;
-  bool has_loop();
-  size_t hash();
-  bool check_correctness();
-  float total_cost() const;
-  int gate_count() const;
-  size_t get_next_special_op_guid();
-  size_t get_special_op_guid();
-  void set_special_op_guid(size_t _special_op_guid);
-  Graph *context_shift(Context *src_ctx, Context *dst_ctx, Context *union_ctx,
-                       RuleParser *rule_parser, bool ignore_toffoli = false);
-  Graph *optimize(float alpha, int budget, bool print_subst, Context *ctx,
-                  const std::string &equiv_file_name,
-                  bool use_simulated_annealing, bool enable_early_stop,
-                  bool use_rotation_merging_in_searching,
-                  GateType target_rotation);
-  void constant_and_rotation_elimination();
-  void rotation_merging(GateType target_rotation);
-  void to_qasm(const std::string &save_filename, bool print_result,
-               bool print_id);
-  void draw_circuit(const std::string &qasm_str,
-                    const std::string &save_filename);
-  size_t get_num_qubits();
-  void print_qubit_ops();
-  Graph *toffoli_flip_greedy(GateType target_rotation, GraphXfer *xfer,
-                             GraphXfer *inverse_xfer);
-  bool xfer_appliable(GraphXfer *xfer, Op op) const;
-  Graph *apply_xfer(GraphXfer *xfer, Op op);
-  void all_ops(std::vector<Op> &ops);
-  void all_edges(std::vector<Edge> &edges);
-  void topology_order_ops(std::vector<Op> &ops) const;
+        inline bool operator<(const Pos &b) const {
+            if (op != b.op)
+                return op < b.op;
+            if (idx != b.idx)
+                return idx < b.idx;
+            return false;
+        }
 
-private:
-  void replace_node(Op oldOp, Op newOp);
-  void remove_node(Op oldOp);
-  void remove_edge(Op srcOp, Op dstOp);
-  uint64_t xor_bitmap(uint64_t src_bitmap, int src_idx, uint64_t dst_bitmap,
-                      int dst_idx);
-  void explore(Pos pos, bool left, std::unordered_set<Pos, PosHash> &covered);
-  void expand(Pos pos, bool left, GateType target_rotation,
-              std::unordered_set<Pos, PosHash> &covered,
-              std::unordered_map<int, Pos> &anchor_point,
-              std::unordered_map<Pos, int, PosHash> pos_to_qubits,
-              std::queue<int> &todo_qubits);
-  void remove(Pos pos, bool left, std::unordered_set<Pos, PosHash> &covered);
-  bool moveable(GateType tp);
-  bool move_forward(Pos &pos, bool left);
-  bool merge_2_rotation_op(Op op_0, Op op_1);
-  Graph *_match_rest_ops(GraphXfer *xfer, int depth, int ignore_depth,
-                         int min_guid) const;
+        Pos &operator=(const Pos &pos) {
+            op = pos.op;
+            idx = pos.idx;
+            return *this;
+        }
 
-private:
-  size_t special_op_guid;
+        Pos(Op op_, int idx_) : op(op_), idx(idx_) {}
 
-public:
-  Context *context;
-  float totalCost;
-  std::map<Op, std::set<Edge, EdgeCompare>, OpCompare> inEdges, outEdges;
-  std::map<Op, ParamType> constant_param_values;
-  std::unordered_map<Op, int, OpHash> qubit_2_idx;
-};
+        Op op;
+        int idx;
+    };
+
+    inline bool operator==(const Pos &a, const Pos &b) {
+        if (a.op != b.op)
+            return false;
+        if (a.idx != b.idx)
+            return false;
+        return true;
+    }
+
+    inline bool operator!=(const Pos &a, const Pos &b) {
+        if (a.op != b.op)
+            return true;
+        if (a.idx != b.idx)
+            return true;
+        return false;
+    }
+
+    class PosHash {
+    public:
+        size_t operator()(const Pos &a) const {
+            std::hash<size_t> hash_fn;
+            OpHash op_hash;
+            return op_hash(a.op) * 17 + hash_fn(a.idx);
+        }
+    };
+
+    class PosCompare {
+    public:
+        bool operator()(const Pos &a, const Pos &b) const {
+            if (a.op != b.op)
+                return a.op < b.op;
+            return a.idx < b.idx;
+        }
+    };
+
+    class Tensor {
+    public:
+        Tensor(void);
+
+        int idx;
+        Op op;
+    };
+
+    struct Edge {
+        Edge(void);
+
+        Edge(const Op &_srcOp, const Op &_dstOp, int _srcIdx, int _dstIdx);
+
+        Edge(const Op &_srcOp, const Op &_dstOp, int _srcIdx, int _dstIdx,
+             int _logical_qubit_idx, int _physical_qubit_idx);
+
+        Op srcOp, dstOp;
+        int srcIdx, dstIdx;
+        int logical_qubit_idx, physical_qubit_idx;
+    };
+
+    struct EdgeCompare {
+        bool operator()(const Edge &a, const Edge &b) const {
+            if (!(a.srcOp == b.srcOp))
+                return a.srcOp < b.srcOp;
+            if (!(a.dstOp == b.dstOp))
+                return a.dstOp < b.dstOp;
+            if (a.srcIdx != b.srcIdx)
+                return a.srcIdx < b.srcIdx;
+            if (a.dstIdx != b.dstIdx)
+                return a.dstIdx < b.dstIdx;
+            return false;
+        };
+    };
+
+    class GraphXfer;
+
+    class Graph {
+    public:
+        Graph(Context *ctx);
+
+        Graph(Context *ctx, const DAG *dag);
+
+        Graph(const Graph &graph);
+
+        void add_edge(const Op &srcOp, const Op &dstOp, int srcIdx, int dstIdx);
+
+        bool has_edge(const Op &srcOp, const Op &dstOp, int srcIdx, int dstIdx) const;
+
+        bool has_loop();
+
+        size_t hash();
+
+        bool check_correctness();
+
+        float total_cost() const;
+
+        int gate_count() const;
+
+        size_t get_next_special_op_guid();
+
+        size_t get_special_op_guid();
+
+        void set_special_op_guid(size_t _special_op_guid);
+
+        Graph *context_shift(Context *src_ctx, Context *dst_ctx, Context *union_ctx,
+                             RuleParser *rule_parser, bool ignore_toffoli = false);
+
+        Graph *optimize(float alpha, int budget, bool print_subst, Context *ctx,
+                        const std::string &equiv_file_name,
+                        bool use_simulated_annealing, bool enable_early_stop,
+                        bool use_rotation_merging_in_searching,
+                        GateType target_rotation);
+
+        void constant_and_rotation_elimination();
+
+        void rotation_merging(GateType target_rotation);
+
+        void to_qasm(const std::string &save_filename, bool print_result,
+                     bool print_id);
+
+        void draw_circuit(const std::string &qasm_str,
+                          const std::string &save_filename);
+
+        size_t get_num_qubits();
+
+        void print_qubit_ops();
+
+        Graph *toffoli_flip_greedy(GateType target_rotation, GraphXfer *xfer,
+                                   GraphXfer *inverse_xfer);
+
+        bool xfer_appliable(GraphXfer *xfer, Op op) const;
+
+        Graph *apply_xfer(GraphXfer *xfer, Op op);
+
+        void all_ops(std::vector<Op> &ops);
+
+        void all_edges(std::vector<Edge> &edges);
+
+        void topology_order_ops(std::vector<Op> &ops) const;
+
+    private:
+        void replace_node(Op oldOp, Op newOp);
+
+        void remove_node(Op oldOp);
+
+        void remove_edge(Op srcOp, Op dstOp);
+
+        uint64_t xor_bitmap(uint64_t src_bitmap, int src_idx, uint64_t dst_bitmap,
+                            int dst_idx);
+
+        void explore(Pos pos, bool left, std::unordered_set<Pos, PosHash> &covered);
+
+        void expand(Pos pos, bool left, GateType target_rotation,
+                    std::unordered_set<Pos, PosHash> &covered,
+                    std::unordered_map<int, Pos> &anchor_point,
+                    std::unordered_map<Pos, int, PosHash> pos_to_qubits,
+                    std::queue<int> &todo_qubits);
+
+        void remove(Pos pos, bool left, std::unordered_set<Pos, PosHash> &covered);
+
+        bool moveable(GateType tp);
+
+        bool move_forward(Pos &pos, bool left);
+
+        bool merge_2_rotation_op(Op op_0, Op op_1);
+
+        Graph *_match_rest_ops(GraphXfer *xfer, int depth, int ignore_depth,
+                               int min_guid) const;
+
+    private:
+        size_t special_op_guid;
+
+    public:
+        Context *context;
+        float totalCost;
+        std::map<Op, std::set<Edge, EdgeCompare>, OpCompare> inEdges, outEdges;
+        std::map<Op, ParamType> constant_param_values;
+        std::unordered_map<Op, int, OpHash> qubit_2_idx;
+    };
 
 }; // namespace quartz

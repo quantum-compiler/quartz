bafc2cbcfc98a9815c377596ac5d2181895aa8cf
xumingkuan <xumingkuan0721@126.com>
2022-03-17 01:20:01 -0400

    Let unique_parameters=true in test_pruning.cpp

--- src/test/test_pruning.cpp
+++ src/test/test_pruning.cpp
@@ -1,25 +1,23 @@
 #include "test_pruning.h"
 
 int main() {
-  test_pruning({GateType::u1, GateType::u2, GateType::u3, GateType::cx, GateType::add},
-               "IBM_with_U3_4_2_", 2, 4, 4, false, 1, true, false, false, false, true);
   test_pruning({GateType::rz, GateType::h, GateType::cx, GateType::x,
-                GateType::add}, "Nam_3_", 3, 2, 3, true, 1, true, true, false, true);
+                GateType::add}, "Nam_3_", 3, 2, 3, true, 1, true, true, false, true, true);
   test_pruning({GateType::rz, GateType::h, GateType::cx, GateType::x,
-                GateType::add}, "Nam_4_", 3, 2, 4, true, 1, true, true, false, true);
+                GateType::add}, "Nam_4_", 3, 2, 4, true, 1, true, true, false, true, true);
   test_pruning({GateType::rz, GateType::h, GateType::cx, GateType::x,
-                GateType::add}, "Nam_5_", 3, 2, 5, true, 1, true, true, false, true);
-  test_pruning({GateType::u1, GateType::u2, GateType::cx,
-                GateType::add}, "IBM_2_", 2, 2, 2, true, 1, true, true, false, true);
-  test_pruning({GateType::u1, GateType::u2, GateType::cx,
-                GateType::add}, "IBM_3_", 2, 2, 3, true, 1, true, true, false, true);
-  test_pruning({GateType::u1, GateType::u2, GateType::cx,
-                GateType::add}, "IBM_4_", 2, 2, 4, true, 1, true, true, false, true);
+                GateType::add}, "Nam_5_", 3, 2, 5, true, 1, true, true, false, true, true);
+  test_pruning({GateType::u1, GateType::u2, GateType::u3, GateType::cx,
+                GateType::add}, "IBM_2_", 2, 5, 2, true, 1, true, true, false, true, true);
+  test_pruning({GateType::u1, GateType::u2, GateType::u3, GateType::cx,
+                GateType::add}, "IBM_3_", 2, 5, 3, true, 1, true, true, false, true, true);
+  test_pruning({GateType::u1, GateType::u2, GateType::u3, GateType::cx,
+                GateType::add}, "IBM_4_", 2, 5, 4, true, 1, true, true, false, true, true);
   test_pruning({GateType::rx, GateType::rz, GateType::cz,
-                GateType::add}, "Rigetti_3_", 3, 2, 3, true, 1, true, true, false, true);
+                GateType::add}, "Rigetti_3_", 3, 2, 3, true, 1, true, true, false, true, true);
   test_pruning({GateType::rx, GateType::rz, GateType::cz,
-                GateType::add}, "Rigetti_4_", 3, 2, 4, true, 1, true, true, false, true);
+                GateType::add}, "Rigetti_4_", 3, 2, 4, true, 1, true, true, false, true, true);
   test_pruning({GateType::rx, GateType::rz, GateType::cz,
-                GateType::add}, "Rigetti_5_", 3, 2, 5, true, 1, true, true, true, false);
+                GateType::add}, "Rigetti_5_", 3, 2, 5, true, 1, true, true, true, false, true);
   return 0;
 }

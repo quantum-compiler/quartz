26bf41badc61a5ae8990c007b88c05cd5898687f
zikun-li <lizikunzk@gmail.com>
2022-03-18 20:26:46 +0000

    Added 'disable_search' parameter to run the experiments

--- src/test/test_nam.cpp
+++ src/test/test_nam.cpp
@@ -5,8 +5,9 @@
 using namespace quartz;
 
 void parse_args(char **argv, int argc, bool &simulated_annealing,
-                bool &early_stop, std::string &input_filename,
-                std::string &output_filename, std::string &eqset_filename) {
+                bool &early_stop, bool &disable_search,
+                std::string &input_filename, std::string &output_filename,
+                std::string &eqset_filename) {
   assert(argv[1] != nullptr);
   input_filename = std::string(argv[1]);
   early_stop = true;
@@ -19,6 +20,10 @@ void parse_args(char **argv, int argc, bool &simulated_annealing,
       eqset_filename = std::string(argv[++i]);
       continue;
     }
+    if (!std::strcmp(argv[i], "--disable_search")) {
+      disable_search = true;
+      continue;
+    }
   }
 }
 
@@ -27,8 +32,10 @@ int main(int argc, char **argv) {
   std::string eqset_fn = "../Nam_5_3_complete_ECC_set.json";
   bool simulated_annealing = false;
   bool early_stop = false;
-  parse_args(argv, argc, simulated_annealing, early_stop, input_fn, output_fn,
-             eqset_fn);
+  bool disable_search = false;
+  parse_args(argv, argc, simulated_annealing, early_stop, disable_search,
+             input_fn, output_fn, eqset_fn);
+  auto fn = input_fn.substr(input_fn.rfind('/') + 1);
 
   // Construct contexts
   Context src_ctx({GateType::h, GateType::ccz, GateType::x, GateType::cx,
@@ -52,12 +59,26 @@ int main(int argc, char **argv) {
       GateType::rz, xfer_pair.first, xfer_pair.second);
   //   graph_before_search->to_qasm(input_fn + ".toffoli_flip", false, false);
 
+  auto end = std::chrono::steady_clock::now();
+  if (disable_search) {
+    std::cout << "Optimization results of Quartz for " << fn
+              << " on Nam's gate set." << std::endl
+              << "Gate count after optimization: "
+              << graph_before_search->total_cost() << ", "
+              << (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                     end - start)
+                         .count() /
+                     1000.0
+              << " seconds." << std::endl;
+
+    return 0;
+  }
+
   // Optimization
   auto graph_after_search = graph_before_search->optimize(
       0.999, 0, false, &dst_ctx, eqset_fn, simulated_annealing, early_stop,
       /*rotation_merging_in_searching*/ false, GateType::rz);
-  auto end = std::chrono::steady_clock::now();
-  auto fn = input_fn.substr(input_fn.rfind('/') + 1);
+  end = std::chrono::steady_clock::now();
   std::cout << "Optimization results of Quartz for " << fn
             << " on Nam's gate set." << std::endl
             << "Gate count after optimization: "

79312b8b8af9e42820f452d6fa73d6e186684993
zikun-li <lizikunzk@gmail.com>
2022-03-21 04:44:50 +0000

    Added some scripts

--- run_ibmq_td_disabled.sh
+++ run_ibmq_td_disabled.sh
@@ -1,28 +1,35 @@
 cd build
 make test_ibmq_td_disabled
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/adder_8.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/adder_8.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_3.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/barenco_tof_3.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_4.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/barenco_tof_4.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_5.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/barenco_tof_5.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_10.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/barenco_tof_10.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/csla_mux_3.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/csla_mux_3.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/csum_mux_9.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/csum_mux_9.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^4_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^4_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^5_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^5_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^6_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^6_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^7_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^7_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^8_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^8_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^9_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^9_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^10_mult.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/gf2^10_mult.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/mod5_4.qasm.output.nrigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod_mult_55.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/mod_mult_55.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod_red_21.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/mod_red_21.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/qcla_adder_10.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/qcla_adder_10.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/qcla_com_7.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/qcla_com_7.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/qcla_mod_7.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/qcla_mod_7.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/rc_adder_6.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/rc_adder_6.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_3.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/tof_3.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_4.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/tof_4.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_5.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/tof_5.qasm.output.rigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_10.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/tof_10.qasm.output.nrigetti &
-./test_ibmq_td_disabled ../circuit/nam-benchmarks/vbe_adder_3.qasm --output ../circuit/nam-benchmarks/output_files/rigetti/vbe_adder_3.qasm.output.rigetti &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/qcla_mod_7.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/qcla_mod_7.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/qcla_mod_7.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_4.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/tof_4.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/tof_4.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod_red_21.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/mod_red_21.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/mod_red_21.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^131_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^131_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^131_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_3.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/barenco_tof_3.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/barenco_tof_3.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/vbe_adder_3.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/vbe_adder_3.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/vbe_adder_3.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/qcla_com_7.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/qcla_com_7.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/qcla_com_7.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^163_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^163_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^163_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^9_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^9_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^9_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/adder_8.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/adder_8.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/adder_8.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^8_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^8_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^8_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^10_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^10_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^10_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_10.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/barenco_tof_10.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/barenco_tof_10.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_10.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/tof_10.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/tof_10.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^6_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^6_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^6_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_4.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/barenco_tof_4.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/barenco_tof_4.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod_mult_55.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/mod_mult_55.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/mod_mult_55.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/barenco_tof_5.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/barenco_tof_5.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/barenco_tof_5.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/csum_mux_9.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/csum_mux_9.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/csum_mux_9.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/csla_mux_3.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/csla_mux_3.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/csla_mux_3.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod_adder_1024.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/mod_adder_1024.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/mod_adder_1024.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/rc_adder_6.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/rc_adder_6.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/rc_adder_6.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^7_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^7_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^7_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^4_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^4_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^4_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^5_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^5_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^5_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_3.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/tof_3.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/tof_3.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^128_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^128_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^128_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^16_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^16_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^16_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^64_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^64_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^64_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/gf2^32_mult.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/gf2^32_mult.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/gf2^32_mult.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/qcla_adder_10.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/qcla_adder_10.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/qcla_adder_10.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/tof_5.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/tof_5.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/tof_5.log &
+./test_ibmq_td_disabled ../circuit/nam-benchmarks/mod5_4.qasm --output ../circuit/nam-benchmarks/output_files/ibmq/mod5_4.qasm.output.ibmq.disable_td > ../circuit/nam-benchmarks/output_files/ibmq/log/mod5_4.log &

10d2518c8f7d03f8947c089ca3861647319d19c7
zikun-li <lizikunzk@gmail.com>
2022-06-21 00:54:39 +0000

    Added a method in Graph to directly read in qasm files, supporting parameterized gates

--- src/test/gen_ecc_set.cpp
+++ src/test/gen_ecc_set.cpp
@@ -8,9 +8,8 @@ using namespace quartz;
 
 void gen_ecc_set(const std::vector<GateType> &supported_gates,
                  const std::string &file_prefix, bool unique_parameters,
-                 int num_qubits,
-                 int num_input_parameters, int max_num_quantum_gates,
-                 int max_num_param_gates = 1) {
+                 int num_qubits, int num_input_parameters,
+                 int max_num_quantum_gates, int max_num_param_gates = 1) {
   Context ctx(supported_gates, num_qubits, num_input_parameters);
   Generator gen(&ctx);
 
@@ -19,12 +18,11 @@ void gen_ecc_set(const std::vector<GateType> &supported_gates,
   Dataset dataset1;
 
   gen.generate(num_qubits, num_input_parameters, 1, max_num_param_gates,
-               &dataset1,        /*verify_equivalences=*/
+               &dataset1,                           /*verify_equivalences=*/
                true, &equiv_set, unique_parameters, /*verbose=*/
                false);
   std::cout << "*** ch(" << file_prefix.substr(0, file_prefix.size() - 5)
-            << ",q=" << file_prefix[file_prefix.size() - 2]
-            << ") = "
+            << ",q=" << file_prefix[file_prefix.size() - 2] << ") = "
             << dataset1.num_total_dags() - 1 /*exclude the empty circuit*/
             << std::endl;
   dataset1.clear();
@@ -32,19 +30,19 @@ void gen_ecc_set(const std::vector<GateType> &supported_gates,
   auto start = std::chrono::steady_clock::now();
   decltype(start - start) verification_time{0};
   gen.generate(num_qubits, num_input_parameters, max_num_quantum_gates,
-               max_num_param_gates, &dataset1, /*verify_equivalences=*/
-               true, &equiv_set, unique_parameters,             /*verbose=*/
+               max_num_param_gates, &dataset1,      /*verify_equivalences=*/
+               true, &equiv_set, unique_parameters, /*verbose=*/
                true, &verification_time);
   dataset1.remove_singletons(&ctx);
   dataset1.save_json(&ctx, file_prefix + "pruning_unverified.json");
 
   auto start2 = std::chrono::steady_clock::now();
   system(("python src/python/verifier/verify_equivalences.py " + file_prefix +
-      "pruning_unverified.json " + file_prefix + "pruning.json")
+          "pruning_unverified.json " + file_prefix + "pruning.json")
              .c_str());
   auto end2 = std::chrono::steady_clock::now();
   verification_time += end2 - start2;
-  equiv_set.clear();  // this is necessary
+  equiv_set.clear(); // this is necessary
   equiv_set.load_json(&ctx, file_prefix + "pruning.json");
   start2 = std::chrono::steady_clock::now();
   equiv_set.simplify(&ctx);
@@ -54,22 +52,22 @@ void gen_ecc_set(const std::vector<GateType> &supported_gates,
 
   std::cout << file_prefix.substr(0, file_prefix.size() - 1)
             << " generated. Running Time (s): "
-            << (double) std::chrono::duration_cast<std::chrono::milliseconds>(
-                end - start)
-                .count() /
-                1000.0
+            << (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                   end - start)
+                       .count() /
+                   1000.0
             << std::endl;
   std::cout << "Pruning Time (s): "
-            << (double) std::chrono::duration_cast<std::chrono::milliseconds>(
-                end2 - start2)
-                .count() /
-                1000.0
+            << (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                   end2 - start2)
+                       .count() /
+                   1000.0
             << std::endl;
   std::cout << "Verification Time (s): "
-            << (double) std::chrono::duration_cast<std::chrono::milliseconds>(
-                verification_time)
-                .count() /
-                1000.0
+            << (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                   verification_time)
+                       .count() /
+                   1000.0
             << std::endl;
   std::cout << "Num_total_dags = " << equiv_set.num_total_dags()
             << ", num_equivalence_classes = "
@@ -79,7 +77,7 @@ void gen_ecc_set(const std::vector<GateType> &supported_gates,
             << file_prefix.substr(0, file_prefix.size() - 1) << " = "
             << (equiv_set.num_total_dags() -
                 equiv_set.num_equivalence_classes()) *
-                2
+                   2
             << std::endl;
 }
 
@@ -106,13 +104,12 @@ int main() {
   //     file_prefix += std::to_string(q);
   //     file_prefix += "_";
   //     gen_ecc_set(
-  //         {GateType::rz, GateType::h, GateType::cx, GateType::x, GateType::add},
-  //         file_prefix, true, q, 2, n);
+  //         {GateType::rz, GateType::h, GateType::cx, GateType::x,
+  //         GateType::add}, file_prefix, true, q, 2, n);
   //   }
   // }
-  gen_ecc_set(
-    {GateType::t, GateType::tdg, GateType::h, GateType::cx, GateType::add},
-    "3_2_5_", true, 3, 2, 5
-  );
+  gen_ecc_set({GateType::t, GateType::tdg, GateType::h, GateType::x,
+               GateType::cx, GateType::add},
+              "3_2_5_", true, 3, 0, 5);
   return 0;
 }

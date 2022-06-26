4b8dcd7aff23a0bbd51105e398fbe288fd43f464
Antony <meiyixuan2000@126.com>
2022-06-26 13:04:08 +0800

    add read

--- experiment/figure_generation/figure2/figure2_data_collection.py
+++ experiment/figure_generation/figure2/figure2_data_collection.py
@@ -1,19 +1,28 @@
 import json
+import random
+
+
+def analyze_circuits(circuit_list):
+    pass
 
 
 def main():
     # input parameters
     circuit_dict_path = "../data/900000_graph.json"
-    connectivity_dict_path = "../data/900000_path.json"
-    num_workers = 1
     total_circuit_count = 10
+    num_workers = 1
 
-    # read json files
+    # read json files and randomly sample a subset of circuits
     with open(circuit_dict_path, 'r') as handle:
         circuit_dict = json.load(handle)
-    with open(connectivity_dict_path, 'r') as handle:
-        connectivity_dict = json.load(handle)
-    print()
+    selected_hash_list = random.sample(list(circuit_dict), total_circuit_count)
+    selected_circuit_list = []
+    for selected_hash in selected_hash_list:
+        selected_circuit_list.append(circuit_dict[selected_hash])
+
+    # compute min #xfers for these circuits
+    # TODO: change this to multi-processing
+    analyze_circuits(selected_circuit_list)
 
 
 if __name__ == '__main__':

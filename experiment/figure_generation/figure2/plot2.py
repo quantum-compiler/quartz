6883850bd009e673e336e71124fd8b30d9185de6
Antony <meiyixuan2000@126.com>
2022-06-27 15:11:35 +0800

    add new plotter

--- experiment/figure_generation/figure2/plot2.py
+++ experiment/figure_generation/figure2/plot2.py
@@ -3,30 +3,31 @@ import pickle
 import matplotlib.pyplot as plt
 
 
-# This file plot cdf.
+# This file plots distribution
 def main():
     with open(f"./final_results.pickle", 'rb') as handle:
         result_dict = pickle.load(file=handle)
 
     # prepare y_list
-    y_list = [0]
-    cumulative_value = 0
+    y_list = []
     for i in range(max(result_dict)):
-        cumulative_value += result_dict[i + 1]
-        y_list.append(cumulative_value)
-    y_list.append(cumulative_value + result_dict[-1])
-    y_list = [y_list[idx] / y_list[-1] for idx in range(len(y_list))]
-    print(y_list)
+        y_list.append(result_dict[i + 1])
+    y_list.append(result_dict[-1])
+    total = sum(y_list)
+    y_list = [y_list[idx] / total for idx in range(len(y_list))]
+    print(total, y_list)
 
     # prepare x_list
-    x_list = list(range(max(result_dict) + 2))
+    x_list = list(range(1, max(result_dict) + 2))
+    for idx in range(len(x_list)):
+        x_list[idx] = f"{x_list[idx]}"
     x_list[-1] = f">{x_list[-2]}"
     print(x_list)
 
     # plot
     plt.xlabel("min #xfers")
     plt.ylabel("percentage")
-    plt.plot(x_list, y_list)
+    plt.bar(x_list, y_list)
     plt.show()
 
 

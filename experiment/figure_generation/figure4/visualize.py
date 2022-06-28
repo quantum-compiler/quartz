33cd2a807647e2e92765707613f0658853870dbf
Antony <meiyixuan2000@126.com>
2022-06-28 18:34:54 +0800

    add visualizer

--- experiment/figure_generation/figure4/visualize.py
+++ experiment/figure_generation/figure4/visualize.py
@@ -4,12 +4,19 @@ from qiskit import QuantumCircuit, transpile
 
 
 def main():
-    input_path = "./path_7"
+    # argument
+    path_length = 7
+
+    # draw
+    input_path = f"./path_{path_length}"
+    output_path = f"./pic_{path_length}"
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     filename_list = os.listdir(input_path)
     for filename in filename_list:
         whole_filename = os.path.join(input_path, filename)
         barenco_tof_3_circ = QuantumCircuit.from_qasm_file(whole_filename)
-        barenco_tof_3_circ.draw(output='mpl', filename=whole_filename + ".jpg")
+        barenco_tof_3_circ.draw(output='mpl', filename=os.path.join(output_path, filename) + ".jpg")
 
 
 if __name__ == '__main__':

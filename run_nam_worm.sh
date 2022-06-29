6d87def8323064ceb2b359a0f8aed192736623ad
zikun-li <lizikunzk@gmail.com>
2022-06-29 20:34:04 +0000

    Added two scripts to perform experiments under nam's gate set using the new APIs

---
+++ run_nam_worm.sh
@@ -0,0 +1,30 @@
+cd src/test
+
+# python test_nam_worm.py adder_8 nam_worm_output &
+# python test_nam_worm.py barenco_tof_3 nam_worm_output &
+# python test_nam_worm.py barenco_tof_4 nam_worm_output &
+# python test_nam_worm.py barenco_tof_5 nam_worm_output &
+# python test_nam_worm.py barenco_tof_10 nam_worm_output &
+# python test_nam_worm.py csla_mux_3 nam_worm_output &
+# python test_nam_worm.py csum_mux_9 nam_worm_output &
+# python test_nam_worm.py gf2^4_mult nam_worm_output &
+# python test_nam_worm.py gf2^5_mult nam_worm_output &
+# python test_nam_worm.py gf2^6_mult nam_worm_output &
+# python test_nam_worm.py gf2^7_mult nam_worm_output &
+# python test_nam_worm.py gf2^8_mult nam_worm_output &
+# python test_nam_worm.py gf2^9_mult nam_worm_output &
+# python test_nam_worm.py gf2^10_mult nam_worm_output &
+# python test_nam_worm.py gf2^16_mult nam_worm_output &
+# python test_nam_worm.py gf2^32_mult nam_worm_output &
+# python test_nam_worm.py mod5_4 nam_worm_output &
+python test_nam_worm.py mod_mult_55 nam_worm_output &
+python test_nam_worm.py mod_red_21 nam_worm_output &
+python test_nam_worm.py qcla_adder_10 nam_worm_output &
+python test_nam_worm.py qcla_com_7 nam_worm_output &
+python test_nam_worm.py qcla_mod_7 nam_worm_output &
+python test_nam_worm.py rc_adder_6 nam_worm_output &
+python test_nam_worm.py tof_3 nam_worm_output &
+python test_nam_worm.py tof_4 nam_worm_output &
+python test_nam_worm.py tof_5 nam_worm_output &
+python test_nam_worm.py tof_10 nam_worm_output &
+python test_nam_worm.py vbe_adder_3 nam_worm_output &
---
+++ src/test/test_nam_rm.py
@@ -0,0 +1,81 @@
+from fileinput import filename
+import quartz
+import time
+import heapq
+import sys
+import os
+
+
+def optimize(context: quartz.QuartzContext,
+             init_circ: quartz.PyGraph,
+             circ_name: str,
+             print_message: bool = True,
+             timeout: int = 86400) -> tuple[int, quartz.PyGraph]:
+    candidate = [(init_circ.gate_count, init_circ.to_qasm_str())]
+    hash_set = set([init_circ.hash()])
+    best_circ: quartz.PyGraph = init_circ
+    best_gate_cnt: int = init_circ.gate_count
+
+    start = time.time()
+    invoke_cnt: int = 0
+
+    while candidate != []:
+        _, circ_qasm_str = heapq.heappop(candidate)
+        circ = quartz.PyGraph.from_qasm_str(context=context,
+                                            qasm_str=circ_qasm_str)
+        all_nodes = circ.all_nodes()
+        for xfer in context.get_xfers():
+            for node in all_nodes:
+                t = time.time()
+                if t - start > timeout:
+                    return best_gate_cnt, best_circ
+
+                invoke_cnt += 1
+                if print_message and invoke_cnt % 100_000_000 == 0:
+                    print(
+                        f"[{circ_name} (rotation merging)] best gate count: {best_gate_cnt}, candidate count: {len(candidate)}, API invoke time: {invoke_cnt}, time cost: {t - start:.3f}s"
+                    )
+
+                new_circ = circ.apply_xfer(xfer=xfer,
+                                           node=node,
+                                           eliminate_rotation=True)
+
+                if new_circ == None:
+                    continue
+
+                new_hash = new_circ.hash()
+                new_cnt = new_circ.gate_count
+                if new_hash not in hash_set:
+                    hash_set.add(new_hash)
+                    heapq.heappush(candidate,
+                                   (new_cnt, new_circ.to_qasm_str()))
+
+                    if new_cnt < best_gate_cnt:
+                        best_gate_cnt = new_cnt
+                        best_circ = new_circ
+
+    return best_gate_cnt, best_circ
+
+
+if __name__ == '__main__':
+    assert (len(sys.argv) > 2)
+
+    circ_name = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    context = quartz.QuartzContext(gate_set=['h', 'cx', 'x', 'rz', 'add'],
+                                   filename='../../Nam_complete_ECC_set.json',
+                                   no_increase=False,
+                                   include_nop=True)
+    circ = quartz.PyGraph.from_qasm(
+        context=context,
+        filename=f"../../circuit/nam_rm_circs/{circ_name}.qasm")
+
+    best_gate_cnt, best_circ = optimize(context, circ, circ_name)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    best_circ.to_qasm(filename=f'{output_dir}/{circ_name}_optimized.qasm')
+
+    with open(f'{output_dir}/results.txt', 'a') as f:
+        f.write(f"{best_gate_cnt}/t/t{circ_name}\n")

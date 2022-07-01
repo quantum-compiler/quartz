97702bd5c296e309a599baea72df46f3db5c1ce6
Antony <meiyixuan2000@126.com>
2022-07-01 22:50:13 +0800

    add code for checking connectivity

--- experiment/figure_generation/figure1/construct_graph.py
+++ experiment/figure_generation/figure1/construct_graph.py
@@ -4,46 +4,55 @@ This is second step of figure1. It generates a graph with node and edges.
 import argparse
 import os
 import json
+import pickle
 
 import quartz
 
 
-class Query:
-    def __init__(self):
-        self.parent_hash = None
-        self.cur_hash = None
-        self.graph = None
+def main():
+    # get neighbor set
+    with open(f"./neighbor_set_13754371184132648763.json", 'r') as handle:
+        multi_hop_neighbor_set = json.load(handle)
+        initial_graph_hash = 13754371184132648763
+    print(len(multi_hop_neighbor_set))
 
-
-def gen_path(rank, qasm_str, max_depth, allow_increase):
-    """
-    Find the minimal number of steps needed to achieve a gate reduction from given circuit.
-    Implemented by BFS.
-    return: -1 if not found under max_depth, min #xfer o.w.
-    """
+    # reconstruct map
+    hash2graph_map = {}  # hash -> [idx, graph, gate_count]
+    edge_map = {}  # idx -> [idx list] (edge set)
     # prepare quartz context
     quartz_context = quartz.QuartzContext(gate_set=['h', 'cx', 't', 'tdg'],
                                           filename="../bfs_verified_simplified.json",
-                                          no_increase=allow_increase, include_nop=False)
+                                          no_increase=False, include_nop=False)
     qasm_parser = quartz.PyQASMParser(context=quartz_context)
-    dag = qasm_parser.load_qasm_str(qasm_str=qasm_str)
-    initial_graph = quartz.PyGraph(context=quartz_context, dag=dag)
-    initial_graph_hash = initial_graph.hash()
-    initial_graph_gate_count = initial_graph.gate_count
-    print(f"[Rank {rank}] New circuit {initial_graph_hash} with {initial_graph_gate_count} gates.")
+    graph_idx = 0
+    original_graph_idx = -1
+    for graph_hash in multi_hop_neighbor_set:
+        if graph_hash == f"{initial_graph_hash}":
+            original_graph_idx = graph_idx
+        graph_qasm = multi_hop_neighbor_set[graph_hash][0]
+        gate_count = multi_hop_neighbor_set[graph_hash][1]
+        dag = qasm_parser.load_qasm_str(qasm_str=graph_qasm)
+        cur_graph = quartz.PyGraph(context=quartz_context, dag=dag)
+        cur_graph_hash = cur_graph.hash()  # graph hash needs to be recomputed for each program
+        cur_graph_gate_count = cur_graph.gate_count
+        assert cur_graph_gate_count == gate_count
+        # append
+        hash2graph_map[cur_graph_hash] = [graph_idx, cur_graph, cur_graph_gate_count]
+        edge_map[graph_idx] = []
+        graph_idx += 1
+    assert not original_graph_idx == -1
 
-    # start BFS search for circuits with fewer gates
-    candidate_queue = [[initial_graph, initial_graph_hash]]
-    visited_hash_set = {initial_graph_hash: 0}
-    # hash -> [graph qasm, gate_count]
-    neighbor_set = {initial_graph_hash: (initial_graph.to_qasm_str(), initial_graph_gate_count)}
-    searched_count = 0
-    while len(candidate_queue) > 0:
+    # check connectivity
+    finished = 0
+    for graph_hash in hash2graph_map:
         # get graph for current loop
-        graph_packet = candidate_queue.pop(0)
-        cur_graph = graph_packet[0]
-        cur_graph_hash = graph_packet[1]
+        graph_packet = hash2graph_map[graph_hash]
+        cur_graph_idx = graph_packet[0]
+        cur_graph = graph_packet[1]
         all_nodes = cur_graph.all_nodes()
+        finished += 1
+        if finished % 100 == 0:
+            print(f"Finished {finished} circuits.")
 
         # apply xfers to get to new graphs
         for i in range(len(all_nodes)):
@@ -53,28 +62,15 @@ def gen_path(rank, qasm_str, max_depth, allow_increase):
             for xfer in applicable_xfers:
                 new_graph = cur_graph.apply_xfer(xfer=quartz_context.get_xfer_from_id(id=xfer), node=node)
                 new_hash = new_graph.hash()
-                new_cnt = new_graph.gate_count
                 # check this circuit
-                if new_hash not in visited_hash_set:
-                    visited_hash_set[new_hash] = visited_hash_set[cur_graph_hash] + 1
-                    if visited_hash_set[new_hash] > max_depth:
-                        return neighbor_set, initial_graph_hash
-                    neighbor_set[new_hash] = (new_graph.to_qasm_str(), new_cnt)
-                    candidate_queue.append([new_graph, new_hash])
-                # this is for logging
-                searched_count += 1
-                if searched_count % 1000 == 0:
-                    print(f"[Rank {rank}] Searched {searched_count} circuits,"
-                          f" now at depth {visited_hash_set[new_hash]}.")
-    assert False
+                if new_hash in hash2graph_map:
+                    new_graph_idx = hash2graph_map[new_hash][0]
+                    edge_map[cur_graph_idx].append(new_graph_idx)
 
-
-def main():
-    # get neighbor set
-    with open(f"./neighbor_set_13754371184132648763.json", 'r') as handle:
-        multi_hop_neighbor_set = json.load(handle)
-        initial_graph_hash = 13754371184132648763
-    print(len(multi_hop_neighbor_set))
+    # output
+    output_file_name = f"./connectivity_graph_{original_graph_idx}.pkl"
+    with open(output_file_name, 'wb') as handle:
+        pickle.dump(edge_map, handle)
 
 
 if __name__ == '__main__':

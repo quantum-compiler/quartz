9034f7671cd60c5b17f928118092a9bfc3b97196
Antony <meiyixuan2000@126.com>
2022-07-02 01:04:26 +0800

    fixed a bug in figure1 old; plot figure 1 new

--- experiment/figure_generation/figure1/gen_data.py
+++ experiment/figure_generation/figure1/gen_data.py
@@ -1,9 +1,5 @@
-"""
-This is first step of figure1. It generates 2-hop neighbor set of tof3.
-"""
 import argparse
 import os
-import json
 
 import quartz
 
@@ -15,7 +11,7 @@ class Query:
         self.graph = None
 
 
-def gen_path(rank, qasm_str, max_depth, allow_increase):
+def gen_path(rank, qasm_str, max_depth):
     """
     Find the minimal number of steps needed to achieve a gate reduction from given circuit.
     Implemented by BFS.
@@ -24,7 +20,7 @@ def gen_path(rank, qasm_str, max_depth, allow_increase):
     # prepare quartz context
     quartz_context = quartz.QuartzContext(gate_set=['h', 'cx', 't', 'tdg'],
                                           filename="../bfs_verified_simplified.json",
-                                          no_increase=not allow_increase, include_nop=False)
+                                          no_increase=True, include_nop=False)
     qasm_parser = quartz.PyQASMParser(context=quartz_context)
     dag = qasm_parser.load_qasm_str(qasm_str=qasm_str)
     initial_graph = quartz.PyGraph(context=quartz_context, dag=dag)
@@ -35,8 +31,11 @@ def gen_path(rank, qasm_str, max_depth, allow_increase):
     # start BFS search for circuits with fewer gates
     candidate_queue = [[initial_graph, initial_graph_hash]]
     visited_hash_set = {initial_graph_hash: 0}
-    # hash -> [graph qasm, gate_count]
-    neighbor_set = {initial_graph_hash: (initial_graph.to_qasm_str(), initial_graph_gate_count)}
+    # hash -> [graph, parent hash, node idx, xfer idx]
+    path_set = {initial_graph_hash: [initial_graph, None, None, None]}
+    distribution_list = []
+    for i in range(max_depth + 1):
+        distribution_list.append(0)
     searched_count = 0
     while len(candidate_queue) > 0:
         # get graph for current loop
@@ -54,34 +53,29 @@ def gen_path(rank, qasm_str, max_depth, allow_increase):
                 new_graph = cur_graph.apply_xfer(xfer=quartz_context.get_xfer_from_id(id=xfer), node=node)
                 new_hash = new_graph.hash()
                 new_cnt = new_graph.gate_count
-                # check this circuit
+                # otherwise we will continue search
                 if new_hash not in visited_hash_set:
                     visited_hash_set[new_hash] = visited_hash_set[cur_graph_hash] + 1
                     if visited_hash_set[new_hash] > max_depth:
-                        return neighbor_set, initial_graph_hash
-                    neighbor_set[new_hash] = (new_graph.to_qasm_str(), new_cnt)
+                        return distribution_list
+                    distribution_list[visited_hash_set[new_hash]] += 1
+                    path_set[new_hash] = [new_graph, cur_graph_hash, i, xfer]
                     candidate_queue.append([new_graph, new_hash])
+
                 # this is for logging
                 searched_count += 1
-                if searched_count % 1000 == 0:
+                if searched_count % 100 == 0:
                     print(f"[Rank {rank}] Searched {searched_count} circuits,"
-                          f" now at depth {visited_hash_set[new_hash]}.")
+                          f" now distribution is {distribution_list}.")
     assert False
 
 
 def main():
-    # parameters
-    gen_depth = 2
-    allow_increase = False
-
     # read in the circuit
     with open(f"../barenco_tof_3.qasm", 'r') as handle:
         qasm_str = handle.read()
-    multi_hop_neighbor_set, initial_graph_hash = gen_path(rank=0, qasm_str=qasm_str,
-                                                          max_depth=gen_depth, allow_increase=allow_increase)
-    print(len(multi_hop_neighbor_set))
-    with open(f"./neighbor_set_{initial_graph_hash}.json", 'w') as f:
-        json.dump(multi_hop_neighbor_set, f, indent=2)
+        final_circuit_count_list = gen_path(rank=0, qasm_str=qasm_str, max_depth=8)
+        print(f"Final circuit count list is {final_circuit_count_list}.")
 
 
 if __name__ == '__main__':

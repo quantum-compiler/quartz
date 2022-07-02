1adfd0370d10bdc87cb88179522149b0db390b48
Antony <meiyixuan2000@126.com>
2022-07-02 14:13:50 +0800

    created final example for figure4

--- experiment/figure_generation/figure4/candidate_generation/verify.py
+++ experiment/figure_generation/figure4/candidate_generation/verify.py
@@ -97,7 +97,7 @@ def main():
         os.makedirs(output_path)
 
     # read in the circuit
-    with open(f"./step4_4_1273.qasm", 'r') as handle:
+    with open(f"./final_5steps.qasm", 'r') as handle:
         qasm_str = handle.read()
         final_depth = gen_path(rank=0, qasm_str=qasm_str, max_depth=8)
         print(f"Final depth is {final_depth}.")

fdc258343f200ba22fbfd7964c48dca525fb19de
Co1lin <mail@co1in.me>
2022-07-04 17:36:18 +0000

    change  run.sh

--- experiment/ppo-new/run.sh
+++ experiment/ppo-new/run.sh
@@ -17,10 +17,9 @@ else
 fi
 
 export OMP_NUM_THREADS=4
-
+# c.time_budget='3:30:00' \
 python ppo.py c=nam_ft c.resume=true c.ckpt_path=pt_iter_115.pt c.ddp_port=23373 \
     'c.input_graphs=[{ name: "gf2^5_mult", path: "../nam_circs/gf2^5_mult.qasm" }]' \
-    c.time_budget='3:30:00' \
     c.mini_batch_size=600 c.k_epochs=10 \
     2>&1 | tee ftlog/gf2^5_mult.log
 

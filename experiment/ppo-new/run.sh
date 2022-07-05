67a1000de29e8e7db5a7d2806a3e2dddb87a0e4d
Co1lin <mail@co1in.me>
2022-07-05 04:08:01 +0000

    change run.sh

--- experiment/ppo-new/run.sh
+++ experiment/ppo-new/run.sh
@@ -16,12 +16,14 @@ else
     echo "Proxy not found."
 fi
 
+mkdir -p ftlog
+
 export OMP_NUM_THREADS=4
 # c.time_budget='3:30:00' \
 python ppo.py c=nam_ft c.resume=true c.ckpt_path=pt_iter_115.pt c.ddp_port=23373 \
-    'c.input_graphs=[{ name: "gf2^5_mult", path: "../nam_circs/gf2^5_mult.qasm" }]' \
+    'c.input_graphs=[{ name: "qcla_com_7", path: "../nam_circs/qcla_com_7.qasm" }]' \
     c.mini_batch_size=600 c.k_epochs=10 \
-    2>&1 | tee ftlog/gf2^5_mult.log
+    2>&1 | tee ftlog/qcla_com_7.log
 
 sleep 10
 

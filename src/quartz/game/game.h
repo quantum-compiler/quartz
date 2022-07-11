3ecae8945a68bf3e953777def7a59daf8e5275a6
Antony <meiyixuan2000@126.com>
2022-07-11 10:56:42 +0800

    add Game

--- src/quartz/game/game.h
+++ src/quartz/game/game.h
@@ -4,5 +4,46 @@
 #include "game_utils.h"
 
 namespace quartz {
+    class State {
 
+    };
+
+    enum class ActionType {
+        Physical = 0, logical = 1, unknown = 2
+    };
+
+    class Action {
+    public:
+        Action() : type(ActionType::unknown), qubit_idx_0(-1), qubit_idx_1(-1) {}
+
+        Action(ActionType _type, int _qubit_idx_0, int _qubit_idx_1) : type(_type), qubit_idx_0(_qubit_idx_0),
+                                                                       qubit_idx_1(_qubit_idx_1) {}
+
+    public:
+        ActionType type;
+        int qubit_idx_0;
+        int qubit_idx_1;
+    };
+
+    class Game {
+    public:
+        Game() = delete;
+
+        Game(const Graph &_graph, std::shared_ptr<DeviceTopologyGraph> &_device) : graph(_graph), device(_device) {
+            /// Game expects that the input graph has been initialized !!!
+            single_qubit_gate_count = simplify_circuit(graph);
+            imp_cost = graph.circuit_implementation_cost(device);
+        }
+
+    public:
+        // state related
+        Graph graph;
+        std::shared_ptr<DeviceTopologyGraph> device;
+
+        // action related
+
+        // reward related
+        int single_qubit_gate_count;
+        double imp_cost;
+    };
 }
---
+++ src/test/test_game.cpp
@@ -0,0 +1,101 @@
+#include "quartz/device/device.h"
+#include "quartz/tasograph/tasograph.h"
+#include "quartz/parser/qasm_parser.h"
+#include "quartz/tasograph/substitution.h"
+#include "quartz/game/game_utils.h"
+#include <iostream>
+
+using namespace quartz;
+using namespace std;
+
+int main() {
+    // tof_3_after_heavy.qasm / t_cx_tdg.qasm
+    string circuit_file_name = "../sabre.qasm";
+    cout << "This is test for add swap on " << circuit_file_name << ".\n";
+
+    // prepare context
+    Context src_ctx({GateType::h, GateType::x, GateType::rz, GateType::add, GateType::swap,
+                     GateType::cx, GateType::input_qubit, GateType::input_param, GateType::t, GateType::tdg,
+                     GateType::s, GateType::sdg});
+    // parse qasm file
+    QASMParser qasm_parser(&src_ctx);
+    DAG *dag = nullptr;
+    if (!qasm_parser.load_qasm(circuit_file_name, dag)) {
+        cout << "Parser failed" << endl;
+        return -1;
+    }
+    Graph graph(&src_ctx, dag);
+    cout << "Circuit initialized\n";
+
+    // initialize clique device
+    auto device = std::make_shared<quartz::SymmetricUniformDevice>(20);
+    for (int i = 0; i < 20; ++i) {
+        for (int j = i + 1; j < 20; ++j) {
+            device->add_edge(i, j);
+        }
+    }
+
+    // print gate count
+    int total_gate_count = graph.gate_count();
+    cout << "Gate count: " << total_gate_count << endl;
+
+    // perform a trivial mapping and print cost
+    auto trivial_graph = graph;
+    trivial_graph.init_physical_mapping(InitialMappingType::TRIVIAL, nullptr,
+                                        -1, false, -1);
+    MappingStatus succeeded = trivial_graph.check_mapping_correctness();
+    if (succeeded == quartz::MappingStatus::VALID) {
+        std::cout << "Trivial Mapping has passed correctness check." << endl;
+    } else {
+        std::cout << "Mapping test failed!" << endl;
+    }
+    double total_cost = trivial_graph.circuit_implementation_cost(device);
+    cout << "Trivial implementation cost is " << total_cost << endl;
+
+    // init sabre mapping and print cost
+    // hyper-parameter search
+    std::vector<int> iter_list{3};
+    std::vector<double> W_list{0.5};
+    std::vector<bool> use_extensive_list{true};
+    double min_sabre_cost = 100000;
+    Graph best_graph = graph;
+    for (const auto &iter_cnt: iter_list) {
+        for (const auto &w_value: W_list) {
+            for (const auto &use_extensive: use_extensive_list) {
+                for (int repeat = 0; repeat < 1; ++repeat) {
+                    auto tmp_graph = graph;
+                    tmp_graph.init_physical_mapping(InitialMappingType::SABRE, device,
+                                                    iter_cnt, use_extensive, w_value);
+                    MappingStatus succeeded_tmp = tmp_graph.check_mapping_correctness();
+                    if (succeeded_tmp != quartz::MappingStatus::VALID) {
+                        std::cout << "Mapping test failed!" << endl;
+                    }
+                    double sabre_cost = tmp_graph.circuit_implementation_cost(device);
+                    if (sabre_cost < min_sabre_cost) {
+                        min_sabre_cost = sabre_cost;
+                        best_graph = tmp_graph;
+                    }
+                }
+            }
+        }
+    }
+    cout << "Sabre search implementation cost is " << min_sabre_cost << endl;
+
+    // simplify circuit
+    int original_gate_count = best_graph.gate_count();
+    int reduction = simplify_circuit(best_graph);
+    int new_gate_count = best_graph.gate_count();
+    cout << "Reduce gate count from " << original_gate_count << " to " << new_gate_count << endl;
+    cout << "#Single qubit gates: " << reduction << endl;
+
+    // find executable gates
+    while (true) {
+        auto executable_gates = find_executable_front_gates(best_graph, device);
+        for (const auto &executable_gate: executable_gates) {
+            cout << "Execute gate " << executable_gate.guid << " with type " << executable_gate.ptr->tp << endl;
+            execute_front_gate(best_graph, executable_gate);
+        }
+        if (is_circuit_finished(best_graph)) break;
+    }
+};
+

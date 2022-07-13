63d37e50aaa72947c637d48df12f2692b363e728
Antony <meiyixuan2000@126.com>
2022-07-13 15:26:14 +0800

    add build script

---
+++ experiment/make_build.sh
@@ -0,0 +1,14 @@
+set -x	# print commands and their arguments as they are executed.
+set -e	# exit immediately if anything you're running returns a non-zero return code
+
+cd /workspace/quartz
+pwd
+
+cd build
+cmake -D CMAKE_INSTALL_PREFIX:PATH=~/usr_local ..
+make -j8 install
+
+cd ..
+cd python
+
+python setup.py build_ext --inplace install

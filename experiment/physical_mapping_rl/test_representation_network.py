6911f44bed27144a274e3c9a457195697ba5e4bc
Antony <meiyixuan2000@126.com>
2022-08-03 18:23:21 +0800

    finished test on representation network

--- experiment/physical_mapping_rl/test_representation_network.py
+++ experiment/physical_mapping_rl/test_representation_network.py
@@ -1,22 +1,26 @@
+import time
+
 from quartz import PySimplePhysicalEnv
 from src.model.representation_network import RepresentationNetworkSimple
 
 
 def main():
-    network = RepresentationNetworkSimple(num_registers=20, device_out_dimension=7,
+    network = RepresentationNetworkSimple(num_registers=20, device_out_dimension=56,
                                           circuit_num_layers=6, num_gate_types=30,
-                                          gate_type_embedding_dim=16, circuit_conv_internal_dim=32,
-                                          circuit_out_dimension=8,
+                                          gate_type_embedding_dim=96, circuit_conv_internal_dim=128,
+                                          circuit_out_dimension=128,
                                           final_mlp_hidden_dimension_ratio=4,
-                                          num_attention_heads=3,
-                                          attention_qk_dimension=16,
-                                          attention_v_dimension=17)
+                                          num_attention_heads=8,
+                                          attention_qk_dimension=64,
+                                          attention_v_dimension=64)
     env = PySimplePhysicalEnv(qasm_file_path="tests/rollout.qasm", backend_type_str="IBM_Q20_TOKYO")
     state = env.get_state()
+    start = time.time()
     rep, attention = network(state)
-    print(f"{rep=}")
+    end = time.time()
+    inference_time = "{:.2f}".format(end - start)
+    print(f"Inference time: {inference_time}s")
     print(f"{rep.shape=}")
-    print(f"{attention=}")
     print(f"{attention.shape=}")
 
 

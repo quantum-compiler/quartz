2a6477a7ee4dbb8d83c381509ab66a4dfb6a46cd
Antony <meiyixuan2000@126.com>
2022-08-05 14:26:38 +0800

    finished PPO networks

---
+++ experiment/physical_mapping_rl/src/model/actor_critic.py
@@ -0,0 +1,40 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self,
+                 register_embedding_dimension,      # dimension of register embedding
+                 ):
+        super(ValueNetwork, self).__init__()
+        hidden_dim1 = pow(2, math.ceil(math.log2(register_embedding_dimension / 3)))
+        hidden_dim2 = pow(2, math.ceil(math.log2(register_embedding_dimension / 5)))
+        hidden_dim3 = pow(2, math.ceil(math.log2(register_embedding_dimension / 11)))
+        self.linear1 = nn.Linear(register_embedding_dimension, hidden_dim1)
+        self.linear2 = nn.Linear(hidden_dim2, hidden_dim3)
+        self.linear3 = nn.Linear(hidden_dim3, 1)
+
+    def forward(self, register_embeddings):
+        """
+        register_embeddings: (num registers, register_embedding_dimension)
+        """
+        state_embedding = torch.sum(register_embeddings, dim=0)
+        hidden1 = self.linear1(state_embedding)
+        hidden2 = self.linear2(hidden1)
+        value = self.linear3(hidden2)
+        return value
+
+
+def PolicyNetworkSimple(attention_score, action_space):
+    # calculate logit for each action
+    logit_mat = attention_score + attention_score.transpose(0, 1)
+    logit_list = []
+    for action in action_space:
+        action_logit = logit_mat[action[0]][action[1]].view(1)
+        logit_list.append(action_logit)
+    action_logits = torch.concat(logit_list, dim=0)
+    action_prob = F.softmax(action_logits, dim=0)
+    return action_prob
--- experiment/physical_mapping_rl/src/model/ppo_network.py
+++ experiment/physical_mapping_rl/src/model/ppo_network.py
@@ -4,37 +4,91 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+from src.model.representation_network import RepresentationNetworkSimple
+from src.model.actor_critic import ValueNetwork, PolicyNetworkSimple
 
-class ValueNetwork(nn.Module):
+from torch.distributions import Categorical
+
+
+class PPONetwork(nn.Module):
     def __init__(self,
-                 register_embedding_dimension,      # dimension of register embedding
+                 # DeviceGNN
+                 num_registers,                     # number of registers
+                 device_out_dimension,              # output dimension of device embedding network
+                 # Circuit GNN
+                 circuit_num_layers,                # number of gnn convolution layers
+                 num_gate_types,                    # number of different gate types
+                 gate_type_embedding_dim,           # dimension of gate type embedding
+                 circuit_conv_internal_dim,         # hidden layer dimension of each convolution layer's MLP
+                 circuit_out_dimension,             # output dimension of final GNN layer
+                 # Multi-head self-attention
+                 final_mlp_hidden_dimension_ratio,  # final MLP's hidden dimension / raw representation dimension
+                 num_attention_heads,               # number of attention heads
+                 attention_qk_dimension,            # dimension of q vector and k vector in attention
+                 attention_v_dimension,             # dimension of v vector in attention
                  ):
-        super(ValueNetwork, self).__init__()
-        hidden_dim1 = pow(2, math.ceil(math.log2(register_embedding_dimension / 3)))
-        hidden_dim2 = pow(2, math.ceil(math.log2(register_embedding_dimension / 5)))
-        hidden_dim3 = pow(2, math.ceil(math.log2(register_embedding_dimension / 11)))
-        self.linear1 = nn.Linear(register_embedding_dimension, hidden_dim1)
-        self.linear2 = nn.Linear(hidden_dim2, hidden_dim3)
-        self.linear3 = nn.Linear(hidden_dim3, 1)
+        super(PPONetwork, self).__init__()
+        self.representation_network = RepresentationNetworkSimple(num_registers=num_registers,
+                                                                  device_out_dimension=device_out_dimension,
+                                                                  circuit_num_layers=circuit_num_layers,
+                                                                  num_gate_types=num_gate_types,
+                                                                  gate_type_embedding_dim=gate_type_embedding_dim,
+                                                                  circuit_conv_internal_dim=circuit_conv_internal_dim,
+                                                                  circuit_out_dimension=circuit_out_dimension,
+                                                                  final_mlp_hidden_dimension_ratio=final_mlp_hidden_dimension_ratio,
+                                                                  num_attention_heads=num_attention_heads,
+                                                                  attention_qk_dimension=attention_qk_dimension,
+                                                                  attention_v_dimension=attention_v_dimension)
+        self.value_network = ValueNetwork(register_embedding_dimension=circuit_out_dimension+device_out_dimension)
+        self.policy_network = PolicyNetworkSimple   # this is a function
 
-    def forward(self, register_embeddings):
+    def policy_forward(self, circuit, circuit_dgl, physical2logical_mapping, action_space):
         """
-        register_embeddings: (num registers, register_embedding_dimension)
+        input:  circuit, circuit_dgl, physical2logical_mapping: observation
+                action_space: action space
+        output: selected action: a tuple (qubit idx 0, qubit idx 1)
+                log probability of selected action
         """
-        state_embedding = torch.sum(register_embeddings, dim=0)
-        hidden1 = self.linear1(state_embedding)
-        hidden2 = self.linear2(hidden1)
-        value = self.linear3(hidden2)
+        # get action probability
+        _, attention_score = self.representation_network(circuit=circuit,
+                                                         circuit_dgl=circuit_dgl,
+                                                         physical2logical_mapping=physical2logical_mapping)
+        action_prob = self.policy_network(attention_score, action_space)
+
+        # sample action and return
+        action_dist = Categorical(probs=action_prob)
+        selected_action_id = action_dist.sample()
+        selected_action_log_prob = action_dist.log_prob(selected_action_id)
+        return action_space[selected_action_id], selected_action_log_prob
+
+    def evaluate_action(self, circuit, circuit_dgl, physical2logical_mapping, action_space, action_id):
+        """
+        input:  circuit, circuit_dgl, physical2logical_mapping: observation
+                action_space: action space
+                action_id: selected action id
+        output: selected action: a tuple (qubit idx 0, qubit idx 1)
+                log probability of selected action
+        """
+        # get action probability
+        _, attention = self.representation_network(circuit=circuit,
+                                                   circuit_dgl=circuit_dgl,
+                                                   physical2logical_mapping=physical2logical_mapping)
+        action_prob = self.policy_network(attention_score, action_space)
+
+        # return statistics of given action
+        action_dist = Categorical(probs=action_prob)
+        action_log_prob = action_dist.log_prob(action_id)
+        dist_entropy = action_dist.entropy()
+        return action_log_prob, dist_entropy
+
+    def value_forward(self, circuit, circuit_dgl, physical2logical_mapping):
+        """
+        input:  circuit, circuit_dgl, physical2logical_mapping: observation
+        output:
+        """
+        # get action probability
+        register_rep, _ = self.representation_network(circuit=circuit,
+                                                      circuit_dgl=circuit_dgl,
+                                                      physical2logical_mapping=physical2logical_mapping)
+        value = self.value_network(register_rep)
         return value
-
-
-def PolicyNetworkSimple(attention_score, action_space):
-    # calculate logit for each action
-    logit_mat = attention_score + attention_score.transpose(0, 1)
-    logit_list = []
-    for action in action_space:
-        action_logit = logit_mat[action[0]][action[1]].view(1)
-        logit_list.append(action_logit)
-    action_logits = torch.concat(logit_list, dim=0)
-    action_prob = F.softmax(action_logits, dim=0)
-    return action_prob

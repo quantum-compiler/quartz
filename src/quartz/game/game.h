762c3f0c99835f4cdca457b7a128c9793f91033d
Antony <meiyixuan2000@126.com>
2022-08-25 12:04:11 +0800

    add execution history check in game

--- src/quartz/game/game.h
+++ src/quartz/game/game.h
@@ -257,6 +257,7 @@ namespace quartz {
             assert(is_circuit_finished(graph));
             assert(original_gate_count == single_qubit_gate_count + executed_logical_gate_count);
             assert(execution_history.size() == swaps_inserted + executed_logical_gate_count);
+            assert(check_execution_history(graph, device, execution_history) == ExecutionHistoryStatus::VALID);
             return original_gate_count + int(SWAPCOST) * swaps_inserted;
         }
 

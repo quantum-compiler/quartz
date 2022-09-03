0aee1b6e18e95bcf53c7e074d5ac8b23b9d34d86
Antony <meiyixuan2000@126.com>
2022-09-03 17:08:41 +0800

    add test for random search of initial mapping

---
+++ experiment/qiskit_transpile_benchmark/test_random_search.py
@@ -0,0 +1,143 @@
+import time
+
+from qiskit import QuantumCircuit
+from qiskit.compiler import transpile
+from qiskit.transpiler import PassManager
+from qiskit.transpiler import CouplingMap
+from qiskit.transpiler.passes import SabreLayout, SabreSwap
+from qiskit.transpiler.passmanager_config import PassManagerConfig
+from customized_sabre_transpiler import sabre_pass_manager
+
+
+def parse_gate_type(tp, circuit):
+    if tp == "cx":
+        return circuit.cx
+    elif tp == "h":
+        return circuit.h
+    elif tp == "t":
+        return circuit.t
+    elif tp == "tdg":
+        return circuit.tdg
+    elif tp == "s":
+        return circuit.s
+    elif tp == "sdg":
+        return circuit.sdg
+    else:
+        raise NotImplementedError
+
+
+def IBM_Q20_Tokyo():
+    # identical to IBM Q20 Tokyo
+    coupling = [
+        # rows
+        [0, 1], [1, 2], [2, 3], [3, 4],
+        [5, 6], [6, 7], [7, 8], [8, 9],
+        [10, 11], [11, 12], [12, 13], [13, 14],
+        [15, 16], [16, 17], [17, 18], [18, 19],
+        # cols
+        [0, 5], [5, 10], [10, 15],
+        [1, 6], [6, 11], [11, 16],
+        [2, 7], [7, 12], [12, 17],
+        [3, 8], [8, 13], [13, 18],
+        [4, 9], [9, 14], [14, 19],
+        # crossings
+        [1, 7], [2, 6],
+        [3, 9], [4, 8],
+        [5, 11], [6, 10],
+        [8, 12], [7, 13],
+        [11, 17], [12, 16],
+        [13, 19], [14, 18]
+    ]
+    reversed_coupling = []
+    for pair in coupling:
+        reversed_coupling.append([pair[1], pair[0]])
+    coupling_map = CouplingMap(couplinglist=coupling + reversed_coupling)
+    return coupling_map
+
+
+def IBM_Q27_Falcon():
+    # identical to IBM Q27 Falcon
+    coupling = [
+        # 1st row
+        [0, 1], [1, 4], [4, 7], [7, 10], [10, 12], [12, 15], [15, 18],
+        [18, 21], [21, 23],
+        # 2nd row
+        [3, 5], [5, 8], [8, 11], [11, 14], [14, 16], [16, 19], [19, 22],
+        [22, 25], [25, 26],
+        # cols
+        [6, 7], [17, 18], [1, 2], [2, 3], [12, 13], [13, 14], [23, 24],
+        [24, 25], [8, 9], [19, 20]
+    ]
+    reversed_coupling = []
+    for pair in coupling:
+        reversed_coupling.append([pair[1], pair[0]])
+    coupling_map = CouplingMap(couplinglist=coupling + reversed_coupling)
+    return coupling_map
+
+
+def run_benchmark(qasm_file_name, device_name):
+    # get device coupling map
+    if device_name == "IBM_Q20_Tokyo":
+        coupling_map, num_regs = IBM_Q20_Tokyo(), 20
+    elif device_name == "IBM_Q27_Falcon":
+        coupling_map, num_regs = IBM_Q27_Falcon(), 27
+    else:
+        raise NotImplementedError
+
+    # parse qasm file into a circuit
+    circuit = QuantumCircuit(num_regs)
+    with open("../../circuit/nam-circuits/qasm_files/" + qasm_file_name) as file:
+        # omit the header
+        file.readline()
+        file.readline()
+        line = file.readline()
+        num_qubits = int(line.split(' ')[1].split(']')[0].split('[')[1])
+        # parse the rest
+        line = file.readline()
+        while line != '':
+            # add to circuit
+            arg_list = line.split(' ')
+            if arg_list[0] == '':
+                arg_list = arg_list[1:]
+            if len(arg_list) == 3:
+                # gate type
+                tp = arg_list[0]
+                # two qubits gate
+                qubit1 = int(arg_list[1].split(']')[0].split('[')[1])
+                qubit2 = int(arg_list[2].split(']')[0].split('[')[1])
+                parse_gate_type(tp=tp, circuit=circuit)(qubit1, qubit2)
+            elif len(arg_list) == 2:
+                # gate type
+                tp = arg_list[0]
+                # single qubit gate
+                qubit1 = int(arg_list[1].split(']')[0].split('[')[1])
+                parse_gate_type(tp=tp, circuit=circuit)(qubit1)
+            else:
+                assert False
+            # read another line
+            line = file.readline()
+
+    # run sabre layout and sabre swap
+    sabre_manager = sabre_pass_manager(PassManagerConfig(coupling_map=coupling_map, layout_method="sabre",
+                                                         routing_method="sabre"))
+    min_num_swaps = 10000
+    while True:
+        sabre_circuit = sabre_manager.run(circuit)
+        sabre_circuit_op_list = dict(sabre_circuit.count_ops())
+        num_swaps = sabre_circuit_op_list["swap"]
+        if num_swaps < min_num_swaps:
+            min_num_swaps = num_swaps
+            print(f"New best initial mapping found with {min_num_swaps} swaps.")
+
+
+def main():
+    # parameters
+    # gf2^E5_mult_after_heavy.qasm, barenco_tof_10_before.qasm, csla_mux_3_after_heavy.qasm
+    # IBM_Q20_Tokyo, IBM_Q27_Falcon
+    qasm_file_name = "gf2^E5_mult_after_heavy.qasm"
+    device_name = "IBM_Q27_Falcon"
+    run_benchmark(qasm_file_name=qasm_file_name, device_name=device_name)
+
+
+if __name__ == '__main__':
+    main()

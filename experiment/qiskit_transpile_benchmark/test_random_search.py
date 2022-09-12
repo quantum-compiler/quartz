63456e9944ef0ce96b4d061a9e28f7b61a2b051e
Antony <meiyixuan2000@126.com>
2022-09-12 20:56:46 +0800

    add test for Q65 Hummingbird in qiskit sabre test

--- experiment/qiskit_transpile_benchmark/test_random_search.py
+++ experiment/qiskit_transpile_benchmark/test_random_search.py
@@ -73,12 +73,43 @@ def IBM_Q27_Falcon():
     return coupling_map
 
 
-def run_benchmark(qasm_file_name, device_name, best_cost):
+def IBM_Q65_Hummingbird():
+    # identical to IBM Q65 Hummingbird
+    coupling = [
+        # first row
+        [0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8], [8, 9],
+        # first layer columns
+        [0, 10], [10, 13], [4, 11], [11, 17], [8, 12], [12, 21],
+        # second row
+        [13, 14], [14, 15], [15, 16], [16, 17], [17, 18], [18, 19], [19, 20], [20, 21], [21, 22], [22, 23],
+        # second layer columns
+        [15, 24], [24, 29], [19, 25], [25, 33], [23, 26], [26, 37],
+        # third layer
+        [27, 28], [28, 29], [29, 30], [30, 31], [31, 32], [32, 33], [33, 34], [34, 35], [35, 36], [36, 37],
+        # third layer columns
+        [27, 38], [38, 41], [31, 39], [39, 45], [35, 40], [40, 49],
+        # fourth layer
+        [41, 42], [42, 43], [43, 44], [44, 45], [45, 46], [46, 47], [47, 48], [48, 49], [49, 50], [50, 51],
+        # fourth layer columns
+        [43, 52], [52, 56], [47, 53], [53, 60], [51, 54], [54, 64],
+        # fifth layer
+        [55, 56], [56, 57], [57, 58], [58, 59], [59, 60], [60, 61], [61, 62], [62, 63], [63, 64],
+    ]
+    reversed_coupling = []
+    for pair in coupling:
+        reversed_coupling.append([pair[1], pair[0]])
+    coupling_map = CouplingMap(couplinglist=coupling + reversed_coupling)
+    return coupling_map
+
+
+def run_benchmark(qasm_file_name, device_name, best_cost, show_mapping):
     # get device coupling map
     if device_name == "IBM_Q20_Tokyo":
         coupling_map, num_regs = IBM_Q20_Tokyo(), 20
     elif device_name == "IBM_Q27_Falcon":
         coupling_map, num_regs = IBM_Q27_Falcon(), 27
+    elif device_name == "IBM_Q65_Hummingbird":
+        coupling_map, num_regs = IBM_Q65_Hummingbird(), 65
     else:
         raise NotImplementedError
 
@@ -141,7 +172,7 @@ def run_benchmark(qasm_file_name, device_name, best_cost):
             assert sabre_circuit_op_list[key] == ori_circuit_op_list[key]
             sabre_gate_count += sabre_circuit_op_list[key]
     assert sabre_gate_count - 3 * sabre_swap_count == ori_gate_count
-    if sabre_swap_count < best_cost:
+    if sabre_swap_count < best_cost and show_mapping:
         print(f"{sabre_swap_count=}")
         cur_layout = sabre_manager.passes()[0]["passes"][0].property_set["layout"]
         layout_dict = cur_layout.get_virtual_bits()
@@ -158,23 +189,30 @@ def run_benchmark(qasm_file_name, device_name, best_cost):
 
 def main():
     # parameters
-    # gf2^E5_mult_after_heavy.qasm, barenco_tof_10_before.qasm, csla_mux_3_after_heavy.qasm
-    # IBM_Q20_Tokyo, IBM_Q27_Falcon
-    qasm_file_name = "gf2^E5_mult_after_heavy.qasm"
-    device_name = "IBM_Q27_Falcon"
+    # gf2^E5_mult_after_heavy.qasm, barenco_tof_10_before.qasm, csla_mux_3_after_heavy.qasm, qcla_adder_10_before.qasm
+    # IBM_Q20_Tokyo, IBM_Q27_Falcon, IBM_Q65_Hummingbird
+    qasm_file_name = "qcla_adder_10_before.qasm"
+    device_name = "IBM_Q65_Hummingbird"
+    num_runs = 100 * 1000
+    show_mapping = False
 
     # run benchmark
     print(f"circuit name: {qasm_file_name}.")
     print(f"device name: {device_name}.")
     min_swap_count = 10000
     step_count = 0
-    while True:
+    original_gate_count = 0
+    while step_count < num_runs:
         step_count += 1
-        data = run_benchmark(qasm_file_name=qasm_file_name, device_name=device_name, best_cost=min_swap_count)
+        data = run_benchmark(qasm_file_name=qasm_file_name, device_name=device_name, best_cost=min_swap_count,
+                             show_mapping=show_mapping)
         ori_gate_count, sabre_gate_count, sabre_swap_count = data
         if sabre_swap_count < min_swap_count:
             min_swap_count = sabre_swap_count
+            original_gate_count = ori_gate_count    # actually this is only set once
             print(f"Best Implementation found: {min_swap_count} swaps. ({step_count} runs)")
+    print(f"Best Implementation found in {step_count} runs: {min_swap_count} swaps."
+          f" (original: {original_gate_count}, after: {original_gate_count + 3 * min_swap_count})")
 
 
 if __name__ == '__main__':

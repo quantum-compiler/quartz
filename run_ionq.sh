b639fef65bddfad2c2ca5da645c8ec5726bf9cb3
Co1lin <mail@co1in.me>
2022-10-11 03:35:53 +0000

    feat: add BFS scripts for ionq

---
+++ run_ionq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+cd src/test
+
+python test_ionq.py dj_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py ghz_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py graphstate_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py grover-noancilla_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py qft_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py qftentangled_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py qpeexact_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py qpeinexact_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py qwalk-noancilla_nativegates_ionq_qiskit_opt0_10_norm ../../ionq_output & sleep 60
+python test_ionq.py shor_15_4_nativegates_ionq_qiskit_opt0_18_norm ../../ionq_output & sleep 60
+python test_ionq.py shor_9_4_nativegates_ionq_qiskit_opt0_18_norm ../../ionq_output & sleep 60

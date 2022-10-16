96592f3fedda70e322c4e598ac811afbc986bab8
zikun-li <lizikunzk@gmail.com>
2022-10-16 06:42:59 +0000

    Modified actor_critic.py in ppo-ablation

--- experiment/ppo-ablation/model/actor_critic.py
+++ experiment/ppo-ablation/model/actor_critic.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Callable, List, Tuple
+from typing import Any, Callable, List, Optional, Tuple
 
 import dgl
 import torch
@@ -15,7 +15,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 class ActorCritic(nn.Module):
     def __init__(
         self,
-        gnn_type: str = 'QGIN',
+        gnn_type: str = "QGIN",
         num_gate_types: int = 20,
         gate_type_embed_dim: int = 16,
         gnn_num_layers: int = 6,
@@ -23,14 +23,15 @@ class ActorCritic(nn.Module):
         gnn_output_dim: int = 32,
         gin_num_mlp_layers: int = 2,
         gin_learn_eps: bool = False,
-        gin_neighbor_pooling_type: str = 'sum',  # sum', 'mean', 'max'
+        gin_neighbor_pooling_type: str = "sum",  # sum', 'mean', 'max'
         gin_graph_pooling_type: str = None,  # 'sum', 'mean', 'max'
         actor_hidden_size: int = 32,
         critic_hidden_size: int = 32,
         action_dim: int = 32,
-        device: torch.device = 'cpu',
+        device: torch.device = "cpu",
         gnn: nn.Module = None,
-        actor: nn.Module = None,
+        actor_gate: nn.Module = None,
+        actor_xfer: nn.Module = None,
         critic: nn.Module = None,
     ) -> None:
         """
@@ -41,17 +42,19 @@ class ActorCritic(nn.Module):
         super().__init__()
         """init the network with existed modules"""
         if gnn is not None:
-            assert actor is not None
+            assert actor_gate is not None
+            assert actor_xfer is not None
             assert critic is not None
             self.gnn = gnn
-            self.actor = actor
+            self.actor_gate = actor_gate
+            self.actor_xfer = actor_xfer
             self.critic = critic
             self.device = device
             return
 
         self.gnn_num_layers = gnn_num_layers
         self.device = device
-        if gnn_type.lower() == 'QGNN'.lower():
+        if gnn_type.lower() == "QGNN".lower():
             self.gnn = QGNN(
                 gnn_num_layers,
                 num_gate_types,
@@ -60,7 +63,7 @@ class ActorCritic(nn.Module):
                 gnn_hidden_dim,
             )
             gnn_output_dim = gnn_hidden_dim
-        elif gnn_type.lower() == 'QGIN'.lower():
+        elif gnn_type.lower() == "QGIN".lower():
             self.gnn = QGIN(
                 num_layers=gnn_num_layers,
                 num_mlp_layers=gin_num_mlp_layers,
@@ -73,9 +76,17 @@ class ActorCritic(nn.Module):
                 graph_pooling_type=gin_graph_pooling_type,
             )
         else:
-            raise NotImplementedError(f'Unknown GNN type {gnn_type}.')
+            raise NotImplementedError(f"Unknown GNN type {gnn_type}.")
 
-        self.actor = MLP(2, gnn_output_dim, actor_hidden_size, action_dim)
+        self.actor_gate = nn.MultiheadAttention(
+            embed_dim=gnn_output_dim,
+            num_heads=1,
+            kdim=gnn_output_dim * gnn_num_layers,
+            vdim=1,
+        )
+        self.actor_xfer = MLP(
+            2, gnn_output_dim * gnn_num_layers, actor_hidden_size, action_dim
+        )
         self.critic = MLP(2, gnn_output_dim, critic_hidden_size, 1)
 
     def ddp_model(self) -> ActorCritic:
@@ -83,26 +94,41 @@ class ActorCritic(nn.Module):
         _ddp_model = ActorCritic(
             device=self.device,
             gnn=DDP(self.gnn, device_ids=[self.device]),
-            actor=DDP(self.actor, device_ids=[self.device]),
+            actor_gate=DDP(self.actor_gate, device_ids=[self.device]),
+            actor_xfer=DDP(self.actor_xfer, device_ids=[self.device]),
             critic=DDP(self.critic, device_ids=[self.device]),
         )
         return _ddp_model
 
-    def forward(self, x: torch.Tensor | dgl.DGLGraph, callee: str) -> torch.Tensor:
+    def forward(
+        self,
+        x: torch.Tensor | dgl.DGLGraph,
+        callee: str,
+        k: Optional[torch.Tensor] = None,
+        v: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
         if callee == self.gnn_name():
             """
             Get tensor representations of nodes in graph(s)
             Return: torch.Tensor (num_nodes, gnn_output_dim)
             """
             return self.gnn(x)
-        elif callee == self.actor_name():
+        elif callee == self.actor_gate_name():
             """
-            Evaluate actions for a node
+            Get the attention scores of nodes in graph(s)
             Args:
-                x: (B, gnn_output_dim)
-            Return: torch.Tensor (B, action_dim)
+                x: torch.Tensor (num_nodes, gnn_output_dim)
+                k: torch.Tensor (num_nodes, gnn_output_dim * gnn_num_layers)
+                v: torch.Tensor (num_nodes, 1)
+            Return: torch.Tensor (num_nodes, 1)
             """
-            return self.actor(x)
+            return self.actor_gate(x, k)[0]
+        elif callee == self.actor_xfer_name():
+            """
+            Get the transformation logits
+            Return: torch.Tensor (num_nodes, action_dim)
+            """
+            return self.actor_xfer(x)
         elif callee == self.critic_name():
             """
             Evaluate nodes in graphs
@@ -112,16 +138,20 @@ class ActorCritic(nn.Module):
             """
             return self.critic(x)
         else:
-            raise NotImplementedError(f'Unexpected callee name: {callee}')
+            raise NotImplementedError(f"Unexpected callee name: {callee}")
 
     @staticmethod
     def gnn_name() -> str:
-        return 'gnn'
+        return "gnn"
 
     @staticmethod
-    def actor_name() -> str:
-        return 'actor'
+    def actor_gate_name() -> str:
+        return "actor_gate"
+
+    @staticmethod
+    def actor_xfer_name() -> str:
+        return "actor_xfer"
 
     @staticmethod
     def critic_name() -> str:
-        return 'critic'
+        return "critic"

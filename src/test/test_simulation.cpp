b14dac09fd2d6e753b4a7ee46f62e3f394e3f4fe
xumingkuan <xumingkuan0721@126.com>
2022-10-23 20:43:25 -0400

    Add MQT benchmarks and code skeleton for test_simulation

---
+++ src/test/test_simulation.cpp
@@ -0,0 +1,18 @@
+#include "quartz/context/context.h"
+#include "quartz/gate/gate_utils.h"
+#include "quartz/parser/qasm_parser.h"
+#include "quartz/tasograph/tasograph.h"
+
+#include <iostream>
+
+using namespace quartz;
+
+int main() {
+  Context ctx(
+      {GateType::h, GateType::u2, GateType::u3, GateType::cx, GateType::cp});
+  QASMParser parser(&ctx);
+  DAG *dag = nullptr;
+  parser.load_qasm("circuit/MQTBench_40q/ae_indep_qiskit_40.qasm", dag);
+  std::cout << "TODO" << std::endl;
+  return 0;
+}

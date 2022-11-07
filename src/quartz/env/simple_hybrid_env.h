dfcf9afbd5b802d30b9500a91758f3c4ea0d8f84
Antony <meiyixuan2000@126.com>
2022-11-07 11:24:30 +0800

    add hybrid env

---
+++ src/quartz/env/simple_hybrid_env.h
@@ -0,0 +1,163 @@
+#pragma once
+
+#include "basic_env.h"
+#include "../parser/qasm_parser.h"
+#include "../supported_devices/supported_devices.h"
+#include "../game/game_buffer.h"
+
+namespace quartz {
+    class SimpleHybridEnv : public BasicEnv {
+    public:
+        SimpleHybridEnv() = delete;
+
+        SimpleHybridEnv(const std::string &qasm_file_path, BackendType backend_type,
+                          int _seed, double _start_from_internal_prob, const std::string &_initial_mapping_file)
+                : game_buffer(_seed), random_generator(_seed) {
+            /// A simple physical environment that does not use curriculum
+            /// Graph    qasm            fixed
+            ///          mapping type    1 x sabre with random init
+            /// Device   topology        fixed
+            // initialize context, graph and device
+            context = std::make_shared<Context>(Context({GateType::h, GateType::cx, GateType::t,
+                                                         GateType::tdg, GateType::input_qubit, GateType::s,
+                                                         GateType::sdg}));
+            QASMParser qasm_parser(&(*context));
+            DAG *dag = nullptr;
+            if (!qasm_parser.load_qasm(qasm_file_path, dag)) {
+                std::cout << "Parser failed" << std::endl;
+                assert(false);
+            }
+            graph = std::make_shared<Graph>(Graph(&(*context), dag));
+            device = GetDevice(backend_type);
+
+            // set randomness (random_generator is initialized above)
+            seed = _seed;
+            uniform01dist = std::uniform_real_distribution<double>(0.0, 1.0);
+
+            // initialize mapping for graph and create game
+            device_reg_count = device->get_num_qubits();
+            initial_mapping_file = _initial_mapping_file;
+            set_initial_mapping(*graph, static_cast<int>(uniform01dist(random_generator) * 1000000),
+                                initial_mapping_file, device_reg_count);
+            assert(graph->check_mapping_correctness() == MappingStatus::VALID);
+            cur_game_ptr = std::make_shared<Game>(Game(*graph, device));
+            while (is_circuit_finished(cur_game_ptr->graph)) {
+                set_initial_mapping(*graph, static_cast<int>(uniform01dist(random_generator) * 1000000),
+                                    initial_mapping_file, device_reg_count);
+                assert(graph->check_mapping_correctness() == MappingStatus::VALID);
+                cur_game_ptr = std::make_shared<Game>(Game(*graph, device));
+            }
+
+            // initialize game buffer (game_buffer is initialize above)
+            start_from_internal_prob = _start_from_internal_prob;
+            game_buffer.save(*cur_game_ptr);
+        }
+
+        SimpleHybridEnv(const SimpleHybridEnv &old_env)
+                : game_buffer(old_env.seed), random_generator(old_env.seed) {
+            // basic parameters
+            // TODO: context here refer to the same object when copy, consider changing it if necessary
+            device_reg_count = old_env.device_reg_count;
+            initial_mapping_file = old_env.initial_mapping_file;
+            GameSearch game_copy = *old_env.cur_game_ptr;
+            cur_game_ptr = std::make_shared<GameSearch>(game_copy);
+            device = old_env.device;
+            context = old_env.context;
+            Graph graph_copy = *old_env.graph;
+            graph = std::make_shared<Graph>(graph_copy);
+
+            // game buffer
+            start_from_internal_prob = old_env.start_from_internal_prob;
+            game_buffer.random_generator = old_env.game_buffer.random_generator;
+            game_buffer.buffer = old_env.game_buffer.buffer;
+
+            // randomness related
+            seed = old_env.seed;
+            random_generator = old_env.random_generator;
+            uniform01dist = old_env.uniform01dist;
+        }
+
+        void reset() override {
+            // generate a random number
+            double _random_val = uniform01dist(random_generator);
+
+            // set new game
+            if (_random_val < start_from_internal_prob) {
+                // start from internal states
+                Game sampled_game = game_buffer.sample();
+                cur_game_ptr = std::make_shared<Game>(sampled_game);
+            } else {
+                // start from initial states
+                set_initial_mapping(*graph, static_cast<int>(uniform01dist(random_generator) * 1000000),
+                                    initial_mapping_file, device_reg_count);
+                assert(graph->check_mapping_correctness() == MappingStatus::VALID);
+                cur_game_ptr = std::make_shared<Game>(Game(*graph, device));
+                while (is_finished()) {
+                    set_initial_mapping(*graph, static_cast<int>(uniform01dist(random_generator) * 1000000),
+                                        initial_mapping_file, device_reg_count);
+                    assert(graph->check_mapping_correctness() == MappingStatus::VALID);
+                    cur_game_ptr = std::make_shared<Game>(Game(*graph, device));
+                }
+            }
+        }
+
+        Reward step(Action action) override {
+            // check whether action is valid
+            assert(action.type == ActionType::PhysicalFront);
+            assert(action.qubit_idx_0 < cur_game_ptr->physical_qubit_num);
+            assert(action.qubit_idx_1 < cur_game_ptr->physical_qubit_num);
+
+            // apply action
+            Reward reward = cur_game_ptr->apply_action(action);
+
+            // save game to buffer if not finished && w.p. 1 / 10 to avoid buffer overflow
+            bool _pass_roll_dice = (uniform01dist(random_generator) < 0.1);
+            if (!is_finished() && _pass_roll_dice) game_buffer.save(*cur_game_ptr);
+
+            // return reward
+            return reward;
+        }
+
+        bool is_finished() override {
+            return is_circuit_finished(cur_game_ptr->graph);
+        }
+
+        int total_cost() override {
+            // call this only when game ends !!!
+            return cur_game_ptr->total_cost();
+        }
+
+        State get_state() override {
+            return cur_game_ptr->state();
+        }
+
+        std::vector<Action> get_action_space() override {
+            // ActionType: Physical Front
+            std::set<Action, ActionCompare> available_action_set = cur_game_ptr->action_space(
+                    ActionType::PhysicalFront);
+            std::vector<Action> action_space;
+            action_space.reserve(available_action_set.size());
+            for (const auto &available_action: available_action_set) {
+                action_space.emplace_back(available_action);
+            }
+            return std::move(action_space);
+        }
+
+    public:
+        // basic parameters
+        int device_reg_count;
+        std::string initial_mapping_file;
+        std::shared_ptr<DeviceTopologyGraph> device;
+        std::shared_ptr<Context> context;
+        std::shared_ptr<Graph> graph;
+
+        // game buffer
+        double start_from_internal_prob;
+        GameBuffer game_buffer;
+
+        // randomness related
+        int seed;
+        std::mt19937 random_generator;
+        std::uniform_real_distribution<double> uniform01dist;
+    };
+}
---
+++ src/test/test_env_hybrid.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include "env/simple_physical_env.h"
+#include "env/simple_hybrid_env.h"
+
+using namespace std;
+using namespace quartz;
+
+int main() {
+    // initialize the environment
+    string circuit_file_name = "../circuit/nam-circuits/qasm_files/gf2^E5_mult_after_heavy.qasm";
+    SimplePhysicalEnv env = SimplePhysicalEnv(circuit_file_name, BackendType::IBM_Q27_FALCON,
+                                              0, 0.8, "../test_mapping_file.txt");
+
+    // apply one step on the environment
+    State state_before = env.get_state();
+    vector<Action> action_space = env.get_action_space();
+    Action selected_action = Action(ActionType::PhysicalFront,
+                                    env.cur_game_ptr->logical2physical[0],
+                                    env.cur_game_ptr->logical2physical[0]);
+    Reward reward = env.step(selected_action);
+    State state_after = env.get_state();
+    bool is_finished = env.is_finished();
+    cout << "Reward 1 is " << reward << ", is finished = " << is_finished << endl;
+
+    // reset the environment to start a new game
+    env.reset();
+    State state_before2 = env.get_state();
+    vector<Action> action_space2 = env.get_action_space();
+    Reward reward2 = env.step(action_space2[0]);
+    State state_after2 = env.get_state();
+    bool is_finished2 = env.is_finished();
+    cout << "Reward 2 is " << reward2 << ", is finished = " << is_finished2 << endl;
+}

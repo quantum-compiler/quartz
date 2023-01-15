4120abb29ea391d2566aa4ead14a5712ba7fcac6
shiyi.c_98 <shicao@student.ethz.ch>
2023-01-15 18:44:20 +0100

    Integrate sim (#68)
    * [simulator] initial attempt to integrate into qartz
    
    * [simulator] fix compilation error, cmake version at least 3.18
    
    * [simulator] set using simulator to be optional
    
    * [simulator] set using simulator optional
    
    * [simulator] do matrix shuffle to guarantee the matrix is for increasing qubit order
    
    * [simulator] matrix shuffle
    
    * [simulator] virtual/physical qubit mapping when applying gates
    
    * [simulator] fix some bugs, tested on one device
    
    * [simulator] fix some bugs, tested on 2-devices
    
    * pre-commit

---
+++ src/sim/examples/test_sim.cc
@@ -0,0 +1,45 @@
+#include <string.h>
+
+#include "sim/circuit.h"
+
+using quartz::GateType;
+
+int main(int argc, char *argv[]) {
+
+  std::string circuit_file;
+  unsigned nqubits;
+  unsigned nlocal;
+  int ndevice;
+  for (int i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], "--import-circuit")) {
+      circuit_file = std::string(argv[++i]);
+      continue;
+    }
+    if (!strcmp(argv[i], "--n")) {
+      nqubits = atoi(argv[++i]);
+      continue;
+    }
+    if (!strcmp(argv[i], "--local")) {
+      nlocal = atoi(argv[++i]);
+      continue;
+    }
+    if (!strcmp(argv[i], "--device")) {
+      ndevice = atoi(argv[++i]);
+      continue;
+    }
+  }
+
+  quartz::Context ctx({GateType::input_qubit, GateType::input_param,
+                       GateType::h, GateType::x, GateType::ry, GateType::u2,
+                       GateType::u3, GateType::cx, GateType::cz, GateType::cp,
+                       GateType::swap});
+  auto seq = quartz::CircuitSeq::from_qasm_file(
+      &ctx, std::string("/home/ubuntu/quartz/circuit/MQTBench_") +
+                std::to_string(nqubits) + "q/" + circuit_file +
+                "_indep_qiskit_" + std::to_string(nqubits) + ".qasm");
+  sim::qcircuit::Circuit<double> circuit(nqubits, nlocal);
+  circuit.compile(seq.get(), &ctx);
+  circuit.simulate(ndevice);
+
+  return true;
+}
---
+++ src/sim/simulator.h
@@ -0,0 +1,50 @@
+#ifndef _SIMULATOR_H_
+#define _SIMULATOR_H_
+
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <type_traits>
+#include <vector>
+
+#include <cuComplex.h>        // cuDoubleComplex
+#include <cuda_runtime_api.h> // cudaMalloc, cudaMemcpy, etc.
+#include <custatevec.h>       // custatevecApplyMatrix
+
+#include "helper.h" // HANDLE_ERROR, HANDLE_CUDA_ERROR
+#include "simgate.h"
+
+#define MAX_DEVICES 4
+#define MAX_QUBIT 30
+
+namespace sim {
+template <typename DT> class SimulatorCuQuantum {
+private:
+  static constexpr auto is_float = std::is_same<DT, float>::value;
+
+public:
+  static constexpr auto cuDT = is_float ? CUDA_C_32F : CUDA_C_64F;
+  static constexpr auto cuCompute =
+      is_float ? CUSTATEVEC_COMPUTE_32F : CUSTATEVEC_COMPUTE_64F;
+
+  SimulatorCuQuantum(unsigned nlocal, unsigned nglobal, int ndevices)
+      : n_qubits(nlocal + nglobal), n_local(nlocal), n_global(nglobal),
+        n_devices(ndevices) {}
+
+  // for simulation
+  bool InitState(std::vector<unsigned> const &init_perm);
+  bool ApplyGate(Gate<DT> &gate, int device_id);
+  bool ApplyShuffle(Gate<DT> &gate);
+  bool Destroy();
+
+  custatevecHandle_t handle_[MAX_DEVICES];
+  void *d_sv[MAX_DEVICES];
+  int n_devices;
+  unsigned n_qubits, n_local, n_global;
+  int devices[MAX_DEVICES];
+  std::vector<unsigned> permutation;
+};
+
+} // namespace sim
+
+#endif // _SIMULATOR_H_

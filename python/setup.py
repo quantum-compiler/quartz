c535ecc1f3a7be0714e555b2fe1053ad77a983ed
Antony <meiyixuan2000@126.com>
2023-03-29 12:17:18 +0800

    launch version 2.5.4 + add some tests to ensure fidelity calculation is correct

--- python/setup.py
+++ python/setup.py
@@ -46,7 +46,7 @@ setup_args = {}
 
 setup(
     name='quartz',
-    version="2.5.3",
+    version="2.5.4",
     description="QuartzPhysical: A learning based quantum circuit layout synthesizer.",
     zip_safe=False,
     install_requires=[],

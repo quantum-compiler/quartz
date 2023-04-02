2ee3aa30c6789f00289f1b6568b394b101f3c404
Co1lin <mail@co1in.me>
2023-04-02 16:31:41 +0000

    ibm_quartz_extra

--- experiment/ppo-new/scripts/ibm_quartz_extra.sh
+++ experiment/ppo-new/scripts/ibm_quartz_extra.sh
@@ -4,24 +4,21 @@ set -ex
 
 export OMP_NUM_THREADS=8
 
-python scripts/ibm_bfs.py realamprandom_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py wstate_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py grover-noancilla_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py graphstate_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qpeexact_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qwalk-noancilla_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py ghz_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py portfoliovqe_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qftentangled_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qpeinexact_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py ae_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qaoa_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py twolocalrandom_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py dj_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py vqe_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qgan_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py qft_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py portfolioqaoa_nativegates_ibm_tket_8 quartz_ibm_outputs online & sleep 30
-python scripts/ibm_bfs.py su2random_nativegates_ibm_tket_8 quartz_ibm_outputs online
+python scripts/ibm_bfs.py dj_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py ghz_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py graphstate_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py wstate_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py vqe_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py qgan_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py qaoa_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py ae_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py qpeexact_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py qpeinexact_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py qft_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py qftentangled_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py portfoliovqe_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py realamprandom_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py twolocalrandom_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online & sleep 30
+python scripts/ibm_bfs.py su2random_nativegates_ibm_tket_8 quartz_ibm_extra_outputs online
 
 echo "Finished!"

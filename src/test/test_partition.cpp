54609a9fcbbcbff3c88ae3fbdcc76747cd5d3a15
zikun-li <lizikunzk@gmail.com>
2023-05-07 20:43:53 +0000

    Modify test_partition.cpp

--- src/test/test_partition.cpp
+++ src/test/test_partition.cpp
@@ -9,10 +9,15 @@ int main() {
                GateType::cx, GateType::x, GateType::rz, GateType::add,
                GateType::neg});
   auto graph = Graph::from_qasm_file(
-      &ctx, "../experiment/circs/nam_circs/barenco_tof_3.qasm");
-  auto subgraphs = graph->topology_partition(1);
+      &ctx, "../experiment/circs/scalability_study/adder_64/adder_64.qasm");
+  auto subgraphs = graph->topology_partition(512);
   std::cout << "number of subgraphs: " << subgraphs.size() << std::endl;
   for (auto subgraph : subgraphs) {
     std::cout << subgraph->to_qasm() << std::endl;
+  }
+  for (int i = 0; i < subgraphs.size(); i++) {
+    subgraphs[i]->to_qasm("../experiment/circs/scalability_study/adder_64/" +
+                              std::to_string(i) + ".qasm",
+                          false, false);
   }
 }

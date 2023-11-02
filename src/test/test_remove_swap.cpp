ce6fe3c038fbf259eef13b2ebea41e8f1804dbd0
xumingkuan <xumingkuan0721@126.com>
2023-11-02 18:38:41 -0400

    [Simulator] Do the simple DP in reversed order and update the benchmark for DP

--- src/test/test_remove_swap.cpp
+++ src/test/test_remove_swap.cpp
@@ -12,7 +12,7 @@ int main() {
   Context ctx({GateType::input_qubit, GateType::input_param, GateType::h,
                GateType::x, GateType::ry, GateType::u2, GateType::u3,
                GateType::cx, GateType::cz, GateType::cp, GateType::swap,
-               GateType::rz});
+               GateType::rz, GateType::rx, GateType::p});
   std::vector<std::string> circuit_names = {"ae",
                                             "dj",
                                             "ghz",
@@ -50,5 +50,49 @@ int main() {
                                   "_no_swap.qasm");
     }
   }
+  /*
+std::vector<std::string> circuit_names = {"bv", "hhl", "ising", "qsvm",
+                                          "vqc"};
+
+std::filesystem::path this_file_path(__FILE__);
+auto circuit_folder =
+    this_file_path.parent_path().parent_path().parent_path().append(
+        "circuit");
+std::vector<int> num_qubits;
+for (int i = 28; i <= 34; i++) {
+  num_qubits.push_back(i);
+}
+for (int num_q1 : num_qubits) {
+  std::cout << num_q1 << " qubits:" << std::endl;
+  for (const auto &circuit : circuit_names) {
+    int num_q = num_q1;
+    if (circuit == std::string("hhl")) {
+      if (num_q == 28) {
+        num_q = 4;
+      } else if (num_q == 29) {
+        num_q = 7;
+      } else if (num_q == 30) {
+        num_q = 9;
+      } else if (num_q == 31) {
+        num_q = 10;
+      } else {
+        continue;
+      }
+    }
+    auto seq = CircuitSeq::from_qasm_file(
+        &ctx, circuit_folder.string() +
+                  (std::string("/NWQBench/") + circuit + "_" +
+                   (circuit == std::string("hhl") ? "" : "n") +
+                   std::to_string(num_q) + ".qasm"));
+    int num_swap = seq->remove_swap_gates();
+    std::cout << circuit << ": " << num_swap << " swap gates removed."
+              << std::endl;
+    seq->to_qasm_file(&ctx, circuit_folder.string() +
+                                (std::string("/NWQBench/") + circuit + "_" +
+                                 (circuit == std::string("hhl") ? "" : "n") +
+                                 std::to_string(num_q) + "_no_swap.qasm"));
+  }
+}
+   */
   return 0;
 }

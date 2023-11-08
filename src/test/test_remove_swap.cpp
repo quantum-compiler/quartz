aa57d71d9039f0cf18504e007b28e588af21a3d9
xumingkuan <xumingkuan0721@126.com>
2023-11-08 15:00:56 -0500

    [simulator] Better file structure for test_remove_swap.cpp

--- src/test/test_remove_swap.cpp
+++ src/test/test_remove_swap.cpp
@@ -1,14 +1,55 @@
 #include "quartz/gate/gate_utils.h"
 #include "quartz/parser/qasm_parser.h"
-#include "quartz/pybind/pybind.h"
 
 #include <filesystem>
 
 using namespace quartz;
+void remove_swap_for_nwq(Context *ctx) {
+  std::vector<std::string> circuit_names = {"bv", "hhl", "ising", "qsvm",
+                                            "vqc"};
 
+  std::filesystem::path this_file_path(__FILE__);
+  auto circuit_folder =
+      this_file_path.parent_path().parent_path().parent_path().append(
+          "circuit");
+  std::vector<int> num_qubits;
+  for (int i = 28; i <= 34; i++) {
+    num_qubits.push_back(i);
+  }
+  num_qubits.push_back(42);
+  for (int num_q1 : num_qubits) {
+    std::cout << num_q1 << " qubits:" << std::endl;
+    for (const auto &circuit : circuit_names) {
+      int num_q = num_q1;
+      if (circuit == std::string("hhl")) {
+        if (num_q == 28) {
+          num_q = 4;
+        } else if (num_q == 29) {
+          num_q = 7;
+        } else if (num_q == 30) {
+          num_q = 9;
+        } else if (num_q == 31) {
+          num_q = 10;
+        } else {
+          continue;
+        }
+      }
+      auto seq = CircuitSeq::from_qasm_file(
+          ctx, circuit_folder.string() +
+                   (std::string("/NWQBench/") + circuit + "_" +
+                    (circuit == std::string("hhl") ? "" : "n") +
+                    std::to_string(num_q) + ".qasm"));
+      int num_swap = seq->remove_swap_gates();
+      std::cout << circuit << ": " << num_swap << " swap gates removed."
+                << std::endl;
+      seq->to_qasm_file(ctx, circuit_folder.string() +
+                                 (std::string("/NWQBench/") + circuit + "_" +
+                                  (circuit == std::string("hhl") ? "" : "n") +
+                                  std::to_string(num_q) + "_no_swap.qasm"));
+    }
+  }
+}
 int main() {
-  init_python_interpreter();
-  PythonInterpreter interpreter;
   Context ctx({GateType::input_qubit, GateType::input_param, GateType::h,
                GateType::x, GateType::ry, GateType::u2, GateType::u3,
                GateType::cx, GateType::cz, GateType::cp, GateType::swap,
@@ -34,6 +75,7 @@ int main() {
   for (int i = 28; i <= 34; i++) {
     num_qubits.push_back(i);
   }
+  num_qubits.push_back(42);
   for (int num_q : num_qubits) {
     std::cout << num_q << " qubits:" << std::endl;
     for (const auto &circuit : circuit_names) {
@@ -50,49 +92,6 @@ int main() {
                                   "_no_swap.qasm");
     }
   }
-  /*
-std::vector<std::string> circuit_names = {"bv", "hhl", "ising", "qsvm",
-                                          "vqc"};
-
-std::filesystem::path this_file_path(__FILE__);
-auto circuit_folder =
-    this_file_path.parent_path().parent_path().parent_path().append(
-        "circuit");
-std::vector<int> num_qubits;
-for (int i = 28; i <= 34; i++) {
-  num_qubits.push_back(i);
-}
-for (int num_q1 : num_qubits) {
-  std::cout << num_q1 << " qubits:" << std::endl;
-  for (const auto &circuit : circuit_names) {
-    int num_q = num_q1;
-    if (circuit == std::string("hhl")) {
-      if (num_q == 28) {
-        num_q = 4;
-      } else if (num_q == 29) {
-        num_q = 7;
-      } else if (num_q == 30) {
-        num_q = 9;
-      } else if (num_q == 31) {
-        num_q = 10;
-      } else {
-        continue;
-      }
-    }
-    auto seq = CircuitSeq::from_qasm_file(
-        &ctx, circuit_folder.string() +
-                  (std::string("/NWQBench/") + circuit + "_" +
-                   (circuit == std::string("hhl") ? "" : "n") +
-                   std::to_string(num_q) + ".qasm"));
-    int num_swap = seq->remove_swap_gates();
-    std::cout << circuit << ": " << num_swap << " swap gates removed."
-              << std::endl;
-    seq->to_qasm_file(&ctx, circuit_folder.string() +
-                                (std::string("/NWQBench/") + circuit + "_" +
-                                 (circuit == std::string("hhl") ? "" : "n") +
-                                 std::to_string(num_q) + "_no_swap.qasm"));
-  }
-}
-   */
+  // remove_swap_for_nwq(&ctx);
   return 0;
 }

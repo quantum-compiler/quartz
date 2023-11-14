9b532873610f0f1fe504392e95acea8449c60ee7
Mingkuan Xu <xumingkuan0721@126.com>
2023-11-13 20:26:37 -0500

    [simulator] Update ILP and DP benchmarks (#146)
    * Add nwq in ILP
    
    * Add num_swaps in benchmark_ilp
    
    * Update DP and ILP benchmarks

--- src/benchmark/dp.cpp
+++ src/benchmark/dp.cpp
@@ -24,8 +24,8 @@ int main() {
       "ae",           "dj",       "ghz",       "graphstate", "qft",
       "qftentangled", "qpeexact", "su2random", "wstate"};
 
-  std::vector<std::string> circuit_names_nwq = {"bv", "hhl", "ising", "qsvm",
-                                                "vqc"};
+  std::vector<std::string> circuit_names_nwq = {"bv", "ising", "qsvm", "vqc",
+                                                "hhl"};
   // 28-34 total qubits, 28 local qubits
   std::vector<int> num_qubits;
   for (int i = 28; i <= 34; i++) {
@@ -45,118 +45,119 @@ int main() {
       },
       /*shared_memory_total_qubits=*/10, /*shared_memory_cacheline_qubits=*/3);
   FILE *fout = fopen("../dp_result.csv", "w");
-  constexpr bool run_nwq = false;
-  for (const auto &circuit : (run_nwq ? circuit_names_nwq : circuit_names)) {
-    fprintf(fout, "%s\n", circuit.c_str());
-    std::cout << circuit << std::endl;
-    for (int num_q : num_qubits) {
-      if (circuit == std::string("hhl")) {
-        if (num_q == 28) {
-          num_q = 4;
-        } else if (num_q == 29) {
-          num_q = 7;
-        } else if (num_q == 30) {
-          num_q = 9;
-        } else if (num_q == 31) {
-          num_q = 10;
-        } else {
-          break;
-        }
-      }
-      // requires running test_remove_swap first
-      auto seq = CircuitSeq::from_qasm_file(
-          &ctx, (run_nwq ? (std::string("../circuit/NWQBench/") + circuit +
-                            "_" + (circuit == std::string("hhl") ? "" : "n") +
-                            std::to_string(num_q) + ".qasm")
-                         : (std::string("../circuit/MQTBench_") +
-                            std::to_string(num_q) + "q/" + circuit +
-                            "_indep_qiskit_" + std::to_string(num_q) +
-                            "_no_swap.qasm")));
-      fprintf(fout, "%d, ", num_q);
-      int answer_start_with = 1;
-      bool has_local_q_at_least_num_q = false;
-      for (int local_q : num_local_qubits) {
-        if (local_q >= num_q) {
-          if (has_local_q_at_least_num_q) {
+  for (int run_nwq = 0; run_nwq <= 1; run_nwq++) {
+    for (const auto &circuit : (run_nwq ? circuit_names_nwq : circuit_names)) {
+      fprintf(fout, "%s\n", circuit.c_str());
+      std::cout << circuit << std::endl;
+      for (int num_q : num_qubits) {
+        if (circuit == std::string("hhl")) {
+          if (num_q == 28) {
+            num_q = 4;
+          } else if (num_q == 29) {
+            num_q = 7;
+          } else if (num_q == 30) {
+            num_q = 9;
+          } else if (num_q == 31) {
+            num_q = 10;
+          } else {
             break;
           }
-          local_q = num_q;
-          has_local_q_at_least_num_q = true;
         }
-        auto t0 = std::chrono::steady_clock::now();
-        std::vector<std::vector<int>> local_qubits;
-        if (local_q == num_q) {
-          std::vector<int> stage(num_q);
-          for (int i = 0; i < num_q; i++) {
-            stage[i] = i;
+        // requires running test_remove_swap first
+        auto seq = CircuitSeq::from_qasm_file(
+            &ctx, (run_nwq ? (std::string("../circuit/NWQBench/") + circuit +
+                              "_" + (circuit == std::string("hhl") ? "" : "n") +
+                              std::to_string(num_q) + ".qasm")
+                           : (std::string("../circuit/MQTBench_") +
+                              std::to_string(num_q) + "q/" + circuit +
+                              "_indep_qiskit_" + std::to_string(num_q) +
+                              "_no_swap.qasm")));
+        fprintf(fout, "%d, ", num_q);
+        int answer_start_with = 1;
+        bool has_local_q_at_least_num_q = false;
+        for (int local_q : num_local_qubits) {
+          if (local_q >= num_q) {
+            if (has_local_q_at_least_num_q) {
+              break;
+            }
+            local_q = num_q;
+            has_local_q_at_least_num_q = true;
           }
-          local_qubits.push_back(stage);
-        } else {
-          local_qubits = compute_local_qubits_with_ilp(
-              *seq, local_q, &ctx, &interpreter, answer_start_with);
+          auto t0 = std::chrono::steady_clock::now();
+          std::vector<std::vector<int>> local_qubits;
+          if (local_q == num_q) {
+            std::vector<int> stage(num_q);
+            for (int i = 0; i < num_q; i++) {
+              stage[i] = i;
+            }
+            local_qubits.push_back(stage);
+          } else {
+            local_qubits = compute_local_qubits_with_ilp(
+                *seq, local_q, &ctx, &interpreter, answer_start_with);
+          }
+          int ilp_result = (int)local_qubits.size();
+          std::cout << local_qubits.size() << " stages." << std::endl;
+          auto t1 = std::chrono::steady_clock::now();
+          auto schedules = get_schedules(
+              *seq, local_qubits, kernel_cost, &ctx,
+              /*attach_single_qubit_gates=*/true,
+              /*use_simple_dp_times=*/1,
+              /*cache_file_name_prefix=*/circuit + std::to_string(num_q) + "_" +
+                  std::to_string(local_q) + "_simple");
+          KernelCostType total_cost = 0;
+          for (auto &schedule : schedules) {
+            // schedule.print_kernel_info();
+            total_cost += schedule.cost_;
+          }
+          fprintf(fout, "%.1f, ", total_cost);
+          fflush(fout);
+          auto t2 = std::chrono::steady_clock::now();
+          schedules = get_schedules(
+              *seq, local_qubits, kernel_cost, &ctx,
+              /*attach_single_qubit_gates=*/true,
+              /*use_simple_dp_times=*/-1,
+              /*cache_file_name_prefix=*/circuit + std::to_string(num_q) + "_" +
+                  std::to_string(local_q) + "_max_weight");
+          total_cost = 0;
+          for (auto &schedule : schedules) {
+            // schedule.print_kernel_info();
+            total_cost += schedule.cost_;
+          }
+          fprintf(fout, "%.1f, ", total_cost);
+          fflush(fout);
+          auto t5 = std::chrono::steady_clock::now();
+          schedules = get_schedules(
+              *seq, local_qubits, kernel_cost, &ctx,
+              /*attach_single_qubit_gates=*/true,
+              /*use_simple_dp_times=*/0,
+              /*cache_file_name_prefix=*/circuit + std::to_string(num_q) + "_" +
+                  std::to_string(local_q) + "_complicated");
+          total_cost = 0;
+          for (auto &schedule : schedules) {
+            // schedule.print_kernel_info();
+            total_cost += schedule.cost_;
+          }
+          fprintf(fout, "%.1f, ", total_cost);
+          fflush(fout);
+          auto t6 = std::chrono::steady_clock::now();
+          fprintf(fout, "%.3f, %.3f, %.3f",
+                  (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                      t2 - t1)
+                          .count() /
+                      1000.0,
+                  (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                      t5 - t2)
+                          .count() /
+                      1000.0,
+                  (double)std::chrono::duration_cast<std::chrono::milliseconds>(
+                      t6 - t5)
+                          .count() /
+                      1000.0);
+          answer_start_with = ilp_result;
         }
-        int ilp_result = (int)local_qubits.size();
-        std::cout << local_qubits.size() << " stages." << std::endl;
-        auto t1 = std::chrono::steady_clock::now();
-        auto schedules = get_schedules(*seq, local_qubits, kernel_cost, &ctx,
-                                       /*attach_single_qubit_gates=*/true,
-                                       /*use_simple_dp_times=*/1,
-                                       /*cache_file_name_prefix=*/circuit +
-                                           std::to_string(num_q) + "_" +
-                                           std::to_string(local_q) + "_simple");
-        KernelCostType total_cost = 0;
-        for (auto &schedule : schedules) {
-          // schedule.print_kernel_info();
-          total_cost += schedule.cost_;
-        }
-        fprintf(fout, "%.1f, ", total_cost);
+        fprintf(fout, "\n");
         fflush(fout);
-        auto t2 = std::chrono::steady_clock::now();
-        schedules = get_schedules(*seq, local_qubits, kernel_cost, &ctx,
-                                  /*attach_single_qubit_gates=*/true,
-                                  /*use_simple_dp_times=*/-1,
-                                  /*cache_file_name_prefix=*/circuit +
-                                      std::to_string(num_q) + "_" +
-                                      std::to_string(local_q) + "_max_weight");
-        total_cost = 0;
-        for (auto &schedule : schedules) {
-          // schedule.print_kernel_info();
-          total_cost += schedule.cost_;
-        }
-        fprintf(fout, "%.1f, ", total_cost);
-        fflush(fout);
-        auto t5 = std::chrono::steady_clock::now();
-        schedules = get_schedules(*seq, local_qubits, kernel_cost, &ctx,
-                                  /*attach_single_qubit_gates=*/true,
-                                  /*use_simple_dp_times=*/0,
-                                  /*cache_file_name_prefix=*/circuit +
-                                      std::to_string(num_q) + "_" +
-                                      std::to_string(local_q) + "_complicated");
-        total_cost = 0;
-        for (auto &schedule : schedules) {
-          // schedule.print_kernel_info();
-          total_cost += schedule.cost_;
-        }
-        fprintf(fout, "%.1f, ", total_cost);
-        fflush(fout);
-        auto t6 = std::chrono::steady_clock::now();
-        fprintf(fout, "%.3f, %.3f, %.3f",
-                (double)std::chrono::duration_cast<std::chrono::milliseconds>(
-                    t2 - t1)
-                        .count() /
-                    1000.0,
-                (double)std::chrono::duration_cast<std::chrono::milliseconds>(
-                    t5 - t2)
-                        .count() /
-                    1000.0,
-                (double)std::chrono::duration_cast<std::chrono::milliseconds>(
-                    t6 - t5)
-                        .count() /
-                    1000.0);
-        answer_start_with = ilp_result;
       }
-      fprintf(fout, "\n");
-      fflush(fout);
     }
   }
   fclose(fout);
--- src/benchmark/ilp_num_stages.cpp
+++ src/benchmark/ilp_num_stages.cpp
@@ -14,7 +14,8 @@
 using namespace quartz;
 
 int num_stages_by_heuristics(CircuitSeq *seq, int num_local_qubits,
-                             std::vector<std::vector<bool>> &local_qubits) {
+                             std::vector<std::vector<bool>> &local_qubits,
+                             int &num_swaps) {
   int num_qubits = seq->get_num_qubits();
   std::unordered_map<CircuitGate *, bool> executed;
   // No initial configuration -- all qubits are global.
@@ -128,6 +129,13 @@ int num_stages_by_heuristics(CircuitSeq *seq, int num_local_qubits,
     }
     std::cout << "}" << std::endl;
     local_qubits.push_back(local_qubit);
+    if (num_stages != 1) {
+      for (int i = 0; i < num_local_qubits; i++) {
+        if (!local_qubits[num_stages - 2][candidate_indices[i]]) {
+          num_swaps++;
+        }
+      }
+    }
   }
   std::cout << num_stages << " stages." << std::endl;
   return num_stages;
@@ -139,76 +147,111 @@ int main() {
   PythonInterpreter interpreter;
   Context ctx({GateType::input_qubit, GateType::input_param, GateType::h,
                GateType::x, GateType::ry, GateType::u2, GateType::u3,
-               GateType::cx, GateType::cz, GateType::cp, GateType::swap});
-  std::vector<std::string> circuit_names = {"ae",
-                                            "dj",
-                                            "ghz",
-                                            "graphstate",
-                                            "qft",
-                                            "qftentangled",
-                                            "qpeexact",
-                                            "qpeinexact",
-                                            "realamprandom",
-                                            "su2random",
-                                            "twolocalrandom",
-                                            "wstate"};
-  // 31 or 42 total qubits, 23-42 local qubits
-  std::vector<int> num_qubits = {31, 42};
-  std::vector<int> num_local_qubits;
-  for (int i = 42; i >= 23; i--) {
-    num_local_qubits.push_back(i);
-  }
+               GateType::cx, GateType::cz, GateType::cp, GateType::swap,
+               GateType::rz, GateType::p, GateType::ccx, GateType::rx});
+  std::vector<std::string> circuit_names = {"ae",         "dj",    "ghz",
+                                            "graphstate", "qft",   "qpeexact",
+                                            "su2random",  "wstate"};
+  std::vector<std::string> circuit_names_nwq = {"bv", "ising", "qsvm", "vqc"};
   FILE *fout = fopen("ilp_result.csv", "w");
-  for (auto circuit : circuit_names) {
-    fprintf(fout, "%s\n", circuit.c_str());
-    for (int num_q : num_qubits) {
-      auto seq = CircuitSeq::from_qasm_file(
-          &ctx, std::string("circuit/MQTBench_") + std::to_string(num_q) +
-                    "q/" + circuit + "_indep_qiskit_" + std::to_string(num_q) +
-                    ".qasm");
-
-      fprintf(fout, "%d, ", num_q);
-      for (int local_q : num_local_qubits) {
-        if (local_q > num_q) {
-          continue;
-        }
-        std::vector<std::vector<bool>> local_qubits_by_heuristics;
-        int heuristics_result = num_stages_by_heuristics(
-            seq.get(), local_q, local_qubits_by_heuristics);
-        fprintf(fout, "%d, ", heuristics_result);
-        fflush(fout);
-      }
-      fprintf(fout, "\n");
+  for (int run_nwq = 0; run_nwq <= 1; run_nwq++) {
+    // 31 or 42 total qubits, 0-23 global qubits
+    std::vector<int> num_qubits = {31, 42};
+    constexpr int kMaxGlobalQubitsFor31 = 16;
+    std::vector<int> num_global_qubits;
+    for (int i = 0; i <= 24; i++) {
+      num_global_qubits.push_back(i);
     }
-    for (int num_q : num_qubits) {
-      auto seq = CircuitSeq::from_qasm_file(
-          &ctx, std::string("circuit/MQTBench_") + std::to_string(num_q) +
-                    "q/" + circuit + "_indep_qiskit_" + std::to_string(num_q) +
-                    ".qasm");
+    for (const auto &circuit : (run_nwq ? circuit_names_nwq : circuit_names)) {
+      fprintf(fout, "%s\n", circuit.c_str());
+      for (int num_q : num_qubits) {
+        // requires running test_remove_swap first
+        auto seq = CircuitSeq::from_qasm_file(
+            &ctx, (run_nwq ? (std::string("circuit/NWQBench/") + circuit + "_" +
+                              (circuit == std::string("hhl") ? "" : "n") +
+                              std::to_string(num_q) + ".qasm")
+                           : (std::string("circuit/MQTBench_") +
+                              std::to_string(num_q) + "q/" + circuit +
+                              "_indep_qiskit_" + std::to_string(num_q) +
+                              "_no_swap.qasm")));
 
-      fprintf(fout, "%d, ", num_q);
-      int answer_start_with = 1;
-      for (int local_q : num_local_qubits) {
-        if (local_q > num_q) {
-          continue;
-        }
-        std::vector<std::vector<int>> local_qubits;
-        local_qubits = compute_local_qubits_with_ilp(
-            *seq, local_q, &ctx, &interpreter, answer_start_with);
-        int ilp_result = (int)local_qubits.size();
-        for (int j = 0; j < ilp_result; j++) {
-          std::cout << "Stage " << j << ": ";
-          std::cout << local_qubits.size() << " local qubits, ";
-          for (int k : local_qubits[j]) {
-            std::cout << k << " ";
+        fprintf(fout, "%d, ", num_q);
+        std::vector<int> n_swaps;
+        for (int global_q : num_global_qubits) {
+          if (num_q == 31 && global_q > kMaxGlobalQubitsFor31) {
+            continue;
           }
-          std::cout << std::endl;
+          std::vector<std::vector<bool>> local_qubits_by_heuristics;
+          int num_swaps = 0;
+          int heuristics_result =
+              num_stages_by_heuristics(seq.get(), num_q - global_q,
+                                       local_qubits_by_heuristics, num_swaps);
+          n_swaps.push_back(num_swaps);
+          fprintf(fout, "%d, ", heuristics_result);
+          fflush(fout);
         }
-        fprintf(fout, "%d, ", ilp_result);
+        fprintf(fout, "\n");
+        fprintf(fout, "%d, ", num_q);
+        for (auto &num_swaps : n_swaps) {
+          fprintf(fout, "%d, ", num_swaps);
+        }
+        fprintf(fout, "\n");
         fflush(fout);
-        answer_start_with = ilp_result;
       }
-      fprintf(fout, "\n");
+      for (int num_q : num_qubits) {
+        // requires running test_remove_swap first
+        auto seq = CircuitSeq::from_qasm_file(
+            &ctx, (run_nwq ? (std::string("circuit/NWQBench/") + circuit + "_" +
+                              (circuit == std::string("hhl") ? "" : "n") +
+                              std::to_string(num_q) + ".qasm")
+                           : (std::string("circuit/MQTBench_") +
+                              std::to_string(num_q) + "q/" + circuit +
+                              "_indep_qiskit_" + std::to_string(num_q) +
+                              "_no_swap.qasm")));
+
+        fprintf(fout, "%d, ", num_q);
+        int answer_start_with = 1;
+        std::vector<int> n_swaps;
+        for (int global_q : num_global_qubits) {
+          if (num_q == 31 && global_q > kMaxGlobalQubitsFor31) {
+            continue;
+          }
+          std::vector<std::vector<int>> local_qubits;
+          local_qubits = compute_local_qubits_with_ilp(
+              *seq, num_q - global_q, &ctx, &interpreter, answer_start_with);
+          int ilp_result = (int)local_qubits.size();
+          int num_swaps = 0;
+          std::vector<bool> prev_local(num_q, false);
+          for (int j = 0; j < ilp_result; j++) {
+            std::cout << "Stage " << j << ": ";
+            std::cout << local_qubits.size() << " local qubits, ";
+            for (int k : local_qubits[j]) {
+              std::cout << k << " ";
+              if (j > 0) {
+                if (!prev_local[k]) {
+                  num_swaps++;
+                }
+              }
+            }
+            prev_local.assign(num_q, false);
+            for (int k : local_qubits[j]) {
+              prev_local[k] = true;
+            }
+            std::cout << std::endl;
+          }
+          n_swaps.push_back(num_swaps);
+          fprintf(fout, "%d, ", ilp_result);
+          fflush(fout);
+          answer_start_with = ilp_result;
+        }
+        fprintf(fout, "\n");
+        fprintf(fout, "%d, ", num_q);
+        for (auto &num_swaps : n_swaps) {
+          fprintf(fout, "%d, ", num_swaps);
+        }
+        fprintf(fout, "\n");
+        fflush(fout);
+      }
     }
   }
   fclose(fout);

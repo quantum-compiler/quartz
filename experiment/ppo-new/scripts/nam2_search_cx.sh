9ef34e95fbd769c430e76e10e3cd12168885df33
Co1lin <mail@co1in.me>
2023-12-28 20:47:48 -0500

    chore: tidy up the scripts for ae

---
+++ experiment/ppo-new/scripts/nam2_search_cx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+date
+pwd
+# ls -lh
+
+mkdir -p ftlog
+
+export OMP_NUM_THREADS=8
+
+# CKPT=ckpts/nam2_rm_iter_xxx.pt
+# BS=2400
+# CIRC=gf2^8_mult
+MEM=${MEM:-30}
+GPU=${GPU:-0}
+
+python ppo.py c=nam2_ft c.resume=true c.ddp_port=$(bash gen_port.sh) c.cost_type=cx_count c.ckpt_path=${CKPT} c.mini_batch_size=${BS} 'c.gpus=['${GPU}']' 'c.input_graphs=[{ name: "'${CIRC}'", path: "../circs/nam_circs/'${CIRC}'.qasm"}]' c.k_epochs=5 c.lr_scheduler=linear c.num_eps_per_iter=64 c.max_eps_len=600 c.vmem_perct_limit=${MEM} c.best_graph_output_dir=nam2_best_graphs > ftlog/tuning_nam2_cx_${CIRC}.log 2>&1 & \
+sleep 5m && \
+python ppo.py c=nam2_test c.resume=true c.ddp_port=$(bash gen_port.sh) c.ckpt_path=${CKPT} 'c.gpus=['${GPU}']' 'c.input_graphs=[{ name: "'${CIRC}'", path: "../circs/nam_circs/'${CIRC}'.qasm"}]' c.num_eps_per_iter=64 c.vmem_perct_limit=${MEM} 2>&1 | tee ftlog/search_nam2_cx_${CIRC}.log
+
+sleep 10

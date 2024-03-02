892aa06cd1616bfe27720c40897d822e77642408
xumingkuan <xumingkuan0721@126.com>
2024-03-01 21:14:00 -0500

    code format

--- src/test/test_context_shift.cpp
+++ src/test/test_context_shift.cpp
@@ -15,9 +15,9 @@ int main() {
       },
       &param_info);
   RuleParser rule_parser({
-      "t q0 = u1 q0 0.25pi",                                  // t -> u1
-      "h q0 = u2 q0 0 pi",                                    // h-> u2
-      "tdg q0 = u1 q0 -0.25pi",                               // tdg -> u1
+      "t q0 = u1 q0 0.25pi",     // t -> u1
+      "h q0 = u2 q0 0 pi",       // h-> u2
+      "tdg q0 = u1 q0 -0.25pi",  // tdg -> u1
       "ccz q0 q1 q2 = cx q1 q2; tdg q2; cx q0 q2; t q2; cx q1 q2; tdg q2; cx "
       "q0 q2; cx q0 q1; tdg q1; cx q0 q1; t q0; t q1; t q2;"  // ccx -> t, tdg,
                                                               // cx

0c9ec4dea06d5db765887806ac4a44009d6b8d6b
Pengyu Liu <pengyul@andrew.cmu.edu>
2024-03-03 22:54:53 -0500

    small bug fix

--- src/wrapper/rust/wrapper.cpp
+++ src/wrapper/rust/wrapper.cpp
@@ -6,6 +6,6 @@
 rust::String optimize(rust::String circ_string, int n_qubits,
                       rust::String cost_func, rust::String ecc_path,
                       rust::String gateset, float timeout) {
-  return optimize_(std::string(circ_string), std::string(cost_func),
+  return optimize_(std::string(circ_string), n_qubits, std::string(cost_func),
                    std::string(ecc_path), std::string(gateset), timeout);
 }

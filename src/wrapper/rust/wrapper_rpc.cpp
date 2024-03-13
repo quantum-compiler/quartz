75c14ddab2169bfff4d5f9e627a79573e1329e0e
Pengyu Liu <pengyul@andrew.cmu.edu>
2024-03-12 20:31:17 -0400

    more configurable

--- src/wrapper/rust/wrapper_rpc.cpp
+++ src/wrapper/rust/wrapper_rpc.cpp
@@ -7,14 +7,18 @@
 using namespace quartz;
 
 int main(int argc, char **argv) {
+  if (argc != 7) {
+    std::cerr << "Usage: " << argv[0]
+              << " <port> <gateset> <eccfile> <cost> <timeout> <nqubits>\n";
+    return 1;
+  }
   int port = atoi(argv[1]);
-
+  float timeout = atof(argv[5]);
+  int nqubits = atoi(argv[6]);
   rpc::server srv(port);
-  auto supercontext = get_context_("Nam", 24,
-                                   "/home/pengyul/quicr/soam/resources/"
-                                   "Nam_4_3_complete_ECC_set.json");
-  srv.bind("optimize", [supercontext](std::string my_circ) {
-    return optimize_(my_circ, "Gate", 10, supercontext);
+  auto supercontext = get_context_(argv[2], nqubits, argv[3]);
+  srv.bind("optimize", [supercontext, timeout, argv](std::string my_circ) {
+    return optimize_(my_circ, argv[4], timeout, supercontext);
   });
   srv.run();
 

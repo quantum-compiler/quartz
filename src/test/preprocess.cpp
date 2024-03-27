85de200e50275fb0f56209840a40b0c054e53881
Pengyu Liu <pengyul@andrew.cmu.edu>
2024-03-27 12:56:57 -0400

    configurable preprocess

--- src/test/preprocess.cpp
+++ src/test/preprocess.cpp
@@ -4,21 +4,39 @@
 
 using namespace quartz;
 
-void parse_args(char **argv, int argc, std::string &input_filename) {
+void parse_args(char **argv, int argc, bool &rm_only, std::string &target_gates,
+                std::string &input_filename) {
   assert(argv[1] != nullptr);
-  input_filename = std::string(argv[1]);
+  assert(argv[2] != nullptr);
+  assert(argv[3] != nullptr);
+  rm_only = std::string(argv[1]) == "rmonly";
+  target_gates = std::string(argv[2]);
+  input_filename = std::string(argv[3]);
 }
 
 int main(int argc, char **argv) {
   std::string input_fn;
+  bool rm_only;
+  std::string target_gates;
+  parse_args(argv, argc, rm_only, target_gates, input_fn);
 
-  parse_args(argv, argc, input_fn);
   std::cout << "input_fn: " << input_fn << std::endl;
   auto fn = input_fn.substr(input_fn.rfind('/') + 1);
   auto param_info = ParamInfo(0);
-  Context src_ctx({GateType::h, GateType::ccz, GateType::x, GateType::cx,
-                   GateType::add, GateType::input_qubit, GateType::input_param},
-                  3, &param_info);
+  auto gates = std::vector<GateType>{};
+  if (!rm_only) {
+    gates = std::vector<GateType>{GateType::h,          GateType::ccz,
+                                  GateType::x,          GateType::cx,
+                                  GateType::add,        GateType::input_qubit,
+                                  GateType::input_param};
+  } else {
+    gates = std::vector<GateType>{GateType::h,          GateType::x,
+                                  GateType::rz,         GateType::add,
+                                  GateType::cx,         GateType::input_qubit,
+                                  GateType::input_param};
+  }
+
+  Context src_ctx(gates, 3, &param_info);
   Context dst_ctx({GateType::h, GateType::x, GateType::rz, GateType::add,
                    GateType::cx, GateType::input_qubit, GateType::input_param},
                   3, &param_info);
@@ -30,7 +48,6 @@ int main(int argc, char **argv) {
   auto union_ctx = union_contexts(&src_ctx, &dst_ctx);
   auto union2_ctx = union_contexts(&dst_ctx, &dst2_ctx);
 
-  auto xfer_pair = GraphXfer::ccz_cx_rz_xfer(&src_ctx, &dst_ctx, &union_ctx);
   // Load qasm file
   QASMParser qasm_parser(&src_ctx);
   CircuitSeq *dag = nullptr;
@@ -39,32 +56,28 @@ int main(int argc, char **argv) {
   }
   Graph graph(&src_ctx, dag);
 
-  std::shared_ptr<Graph> graph_before_search;
+  std::shared_ptr<Graph> newGraph;
 
   // Greedy toffoli flip
-  graph_before_search = graph.toffoli_flip_greedy(GateType::rz, xfer_pair.first,
-                                                  xfer_pair.second);
-  // graph_before_search->to_qasm(input_fn + ".toffoli_flip", false, false);
-  std::cout << "toffoli flip + rotation merging done" << std::endl;
+  if (!rm_only) {
+    auto xfer_pair = GraphXfer::ccz_cx_rz_xfer(&src_ctx, &dst_ctx, &union_ctx);
 
+    newGraph = graph.toffoli_flip_greedy(GateType::rz, xfer_pair.first,
+                                         xfer_pair.second);
+    // graph_before_search->to_qasm(input_fn + ".toffoli_flip", false, false);
+    std::cout << "toffoli flip + rotation merging done" << std::endl;
+  } else {
+    graph.rotation_merging(GateType::rz);
+    newGraph =
+        std::make_shared<Graph>(&dst_ctx, graph.to_circuit_sequence().get());
+  }
   // O(n) rz -> t
-  graph_before_search->context = &union2_ctx;
-  auto graph_seq = graph_before_search->to_circuit_sequence();
-  auto rz_to_t_seq = graph_seq->get_rz_to_t(&union2_ctx);
-  auto newGraph = std::make_shared<Graph>(&dst2_ctx, rz_to_t_seq.get());
-  // O(n^2) rz -> t
-
-  //   std::cout << "rz -> t done" << std::endl;
-
-  //   std::cout << "Optimization results of Quartz for " << fn
-  //             << " on Clifford+T gate set."
-  //             << " Gate count after optimization: " << newGraph->gate_count()
-  //             << ", "
-  //             << "T Count: "
-  //             << newGraph->specific_gate_count(GateType::t) +
-  //                    newGraph->specific_gate_count(GateType::tdg)
-  //             << ", "
-  //             << "Circuit depth: " << newGraph->circuit_depth();
+  if (target_gates == "clifford") {
+    newGraph->context = &union2_ctx;
+    auto graph_seq = newGraph->to_circuit_sequence();
+    auto rz_to_t_seq = graph_seq->get_rz_to_t(&union2_ctx);
+    auto newGraph = std::make_shared<Graph>(&dst2_ctx, rz_to_t_seq.get());
+  }
 
   newGraph->to_qasm(input_fn + ".optimized", false, false);
   return 0;

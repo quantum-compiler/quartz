dcaac2ee31e138afdb75f95d1d6f7e09824bf5a3
xumingkuan <xumingkuan0721@126.com>
2024-05-09 18:20:19 -0400

    Fix ECC Set name

--- src/test/test_optimization.cpp
+++ src/test/test_optimization.cpp
@@ -9,12 +9,10 @@ using namespace quartz;
 int main() {
   ParamInfo param_info;
   Context ctx({GateType::input_qubit, GateType::input_param, GateType::cx,
-               GateType::h, GateType::s, GateType::t, GateType::tdg,
-               GateType::x, GateType::add, GateType::z},
+               GateType::h, GateType::t, GateType::tdg, GateType::x,
+               GateType::add},
               &param_info);
-  //   test_optimization(&ctx, "circuit/example-circuits/voqc_fig5.qasm",
-  //                     "cmake-build-debug/bfs_verified.json");
   test_optimization(&ctx, "circuit/example-circuits/barenco_tof_3.qasm",
-                    "Nam_3_3_complete_ECC_set.json",
+                    "eccset/Clifford_T_5_3_complete_ECC_set.json",
                     /*timeout=*/10, "logs/barenco_tof_3_");
 }

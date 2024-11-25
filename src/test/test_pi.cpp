75a342a714ee42c356b5068d7b998e24f9455c94
scottwe <scott.wesley@dal.ca>
2024-11-24 22:27:06 -0400

    Fixed test_pi to work with new parser

--- src/test/test_pi.cpp
+++ src/test/test_pi.cpp
@@ -20,11 +20,18 @@ int main() {
   CircuitSeq dag2(1);
   dag2.add_gate({0}, {p2}, ctx.get_gate(GateType::rx), &ctx);
 
-  auto c1 = dag1.to_qasm_style_string(&ctx);
-  auto c2 = dag2.to_qasm_style_string(&ctx);
-  if (c1 != c2) {
-    std::cout << "Failed to evaluate pi gate." << std::endl;
-    assert(false);
+  auto mat1 = dag1.get_matrix(&ctx);
+  auto mat2 = dag2.get_matrix(&ctx);
+
+  assert(mat1.size() == mat2.size());
+  for (int i = 0; i < mat1.size(); ++i) {
+    assert(mat1[i].size() == mat2[i].size());
+    for (int j = 0; j < mat1[i].size(); ++j) {
+      if (mat1[i][j] != mat2[i][j]) {
+        std::cout << "Disagree at " << i << ", " << j << "." << std::endl;
+        assert(false);
+      }
+    }
   }
 
   // Working directory is cmake-build-debug/ here.

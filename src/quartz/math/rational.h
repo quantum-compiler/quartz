d766ec796247b641352de02f69426698166361b3
xumingkuan <xumingkuan0721@126.com>
2025-04-18 18:09:53 -0400

    Add a compile flag to use rational times PI for parameters

--- src/quartz/math/rational.h
+++ src/quartz/math/rational.h
@@ -299,6 +299,8 @@ class Rational {
   // helper functions
   [[nodiscard]] bool is_zero() const { return a.is_zero(); }
   [[nodiscard]] bool is_neg() const { return a.is_neg(); }
+  [[nodiscard]] bool is_odd() const { return a.is_odd() && b == Int(1); }
+  [[nodiscard]] bool is_even() const { return !a.is_odd() && b == Int(1); }
   [[nodiscard]] const Int &numerator() const { return a; }
   [[nodiscard]] const Int &denominator() const { return b; }
 
